b7633391e600ad8840b823adaf73c42cc72fa275
kevindehecker <kevindehecker@hotmail.com>
2017-11-01 22:20:26 +0100

    Small protocol for led driver

--- Arduino/ledriver/leddriver/leddriver.ino
+++ Arduino/ledriver/leddriver/leddriver.ino
@@ -1,20 +1,42 @@
 int ledPin = 9;
-int pwm_value = 100;
-String inputString = "";
-boolean stringComplete  = false;
+unsigned char pwm_value = 50;
+
 void setup() {
     Serial.begin(115200);    
-    inputString.reserve(200);
 }
 
 void loop() {
+  if (pwm_value > 150) {
+    pwm_value = 150;
+  }
   // put your main code here, to run repeatedly:
   analogWrite(ledPin, pwm_value);
 
+  static int state = 0;
 
+  while (Serial.available()) {  
+    unsigned char b = Serial.read();      
 
-  
-    while (Serial.available()) {  
-      pwm_value= (char)Serial.read();      
-    }
+  switch(state) {
+    case 0:
+      if (b == 66)
+        state++;
+      break;
+   case 1:
+      if (b == 116)
+        state++;
+      else
+        state =0;
+      break;
+   case 2:
+      pwm_value = b;
+      state = 0;
+      break;
+  default:
+      state = 0;
+    break;
+  }
+    
+  }
+  delay(20);
 }
