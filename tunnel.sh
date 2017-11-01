e4380d6fb21378fca6c23df1112d87319314adc8
Kevindehecker <kevindehecker@hotmail.com>
2017-11-01 20:02:29 +0000

    update tunnel ip to ubussh server

--- tunnel.sh
+++ tunnel.sh
@@ -3,7 +3,8 @@
 while [ 1 ]; do
 	sleep 10s
 	echo starting reverse ssh tunnel
-	ssh -i /home/slamdunk/.ssh/id_rsa -NR 16667:localhost:22 houjebek@dinstech.nl -p 16666
+	/usr/local/bin/autossh -M 20000 -i /home/slamdunk/.ssh/id_rsa -NR 16667:localhost:22 houjebek@dinstech.nl -p 16666
+ 	#ssh -i /home/slamdunk/.ssh/id_rsa -NR 16667:localhost:22 houjebek@dinstech.nl -p 16666
 	#configure firewall to forward port 16666 to the correct nat ip
 	#from that ip, do ssh slamdunk@localhost -p 16667
 	#Add id_rsa.pub to the authorized_keys e.g. with ssh-copy-id
