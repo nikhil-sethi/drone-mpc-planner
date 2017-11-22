3a3dd7ebb58c52dd1604236e9c72ea58483d3906
kevindehecker <kevindehecker@hotmail.com>
2017-11-22 21:53:41 +0100

    oops

--- autoreboot.py
+++ autoreboot.py
@@ -6,12 +6,14 @@ import datetime
 import time
 
 last_reboot_time = datetime.datetime.now()
+print('starting autorebooter' + last_reboot_time)
 while(1):
     now = datetime.datetime.now()    
     diff = (now - last_reboot_time).total_seconds()
-    if now.hour == 14 and last_reboot_time.day != now.day:
-    #   print('Reboot now: ' + str(diff))
+    if now.minute == 14 and last_reboot_time.hour != now.hour:
+        print('Reboot now: ' + str(diff))
         subprocess.call('shutdown -r -t 60', shell=True)
+        exit(0)
     #else:
         #print('Current: ' + str(now.day) + ' reboot: ' + str(last_reboot_time.day))
     time.sleep(10)
