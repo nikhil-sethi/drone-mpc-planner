dc004ead1581e2be8f2426e3ef6c36e106498c84
Kevindehecker <kevindehecker@hotmail.com>
2018-03-06 22:12:44 +0100

    remote connection works

--- tunnel.sh
+++ tunnel.sh
@@ -4,9 +4,10 @@ echo waiting 10s for starting tunnel
 while [ 1 ]; do
 	sleep 10s
 	echo starting tunnel 1!
-	/usr/local/bin/autossh -M 20000 -i /home/slamdunk/.ssh/id_rsa -NR 16667:localhost:22 houjebek@dinstech.nl -p 16666
+	/usr/bin/autossh -M 20000 -i /home/pats/.ssh/id_rsa -NR 16667:localhost:22 houjebek@dinstech.nl -p 16666
  	#ssh -i /home/slamdunk/.ssh/id_rsa -NR 16667:localhost:22 houjebek@dinstech.nl -p 16666
 	#configure firewall to forward port 16666 to the correct nat ip
 	#from that ip, do ssh slamdunk@localhost -p 16667
 	#Add id_rsa.pub to the authorized_keys e.g. with ssh-copy-id
+	#ssh-copy-id -i ~/.ssh/id_rsa.pub kevin@131.180.117.41
 done
