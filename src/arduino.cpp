36085493e0dec3077f5ab7ee33379ef176eacb97
kevindehecker <kevindehecker@hotmail.com>
2018-04-28 14:42:52 +0200

    oops

---
+++ src/arduino.cpp
@@ -0,0 +1,111 @@
+#include "arduino.h"
+
+
+int notconnected;
+
+void Arduino::init(bool fromfile) {
+    // setup connection with Arduino
+    baudrate = 115200;
+    notconnected = RS232_OpenComport(baudrate);
+
+    if (notconnected && !fromfile && !INSECT_DATA_LOGGING_MODE) {
+        printf("Arduino failed.\n");
+        exit(1);
+    }
+
+    thread_nrf = std::thread(&Arduino::workerThread,this);
+
+    std::cout << "led power: " << ledpower << std::endl;
+}
+
+void Arduino::workerThread(void) {
+    std::cout << "Send nrf thread started!" << std::endl;
+    while (!exitSendThread) {
+        g_lockData.lock();
+        sendData();
+        g_lockData.unlock();
+        usleep(20000);
+    }
+}
+
+void Arduino::sendData(void) {
+
+    if (false) {
+        //tmp led power hack
+        char buff[3];
+        buff[0] = 'B';
+        buff[1] = 't';
+        buff[2] = ledpower;
+        if (!notconnected) {
+            RS232_SendBuf( (unsigned char*) buff, 3);
+        }
+        usleep(100000);
+    } else {
+
+        char buff[64];
+        sprintf( (char*) buff,"%u,%u,%u,%u,%u,%u,0,0,0,0,0,0\n",throttle,roll,pitch,yaw,mode,ledpower);
+        if (!notconnected) {
+            RS232_SendBuf( (unsigned char*) buff, 63);
+        }
+
+        //if ( data->valid ) {
+        //std::setprecision(2);
+        //std::cout << data->posY << " " << data->velY << " - ";
+        //std::cout << "JoyCommands:" << std::string(buff) << std::flush;
+        //    (*_logger) << "JoyCommands:" << std::string(buff) << std::flush;
+
+        //}
+
+        if (!notconnected) {
+            unsigned char inbuf[1];
+            inbuf[1] = 0;
+            std::stringstream tmp;
+            int n = 1;
+            int totn = 0;
+            while (n)    {
+                n = RS232_PollComport(inbuf,1);
+                if (n > 0) {
+                    tmp << inbuf[0];
+                    totn += n;
+                }
+            }
+            if (totn > 0) {
+                //std::cout << "Arduino: " << totn << ": " << tmp.str() << std::endl;
+            }
+        }
+
+        //TODO: disable uart polling after bind confirmed!
+        //check if input string is the same as the commanded strning, if so bind was succesfull!
+        //	for (int i = 0 to tmp.count;i++) {
+        //if tmp.str.c_str[i] == buff[i] ...
+        //	}
+
+    }
+}
+
+void Arduino::rebind(void){
+
+    if (rebindValue) {
+        rebindValue=0;
+
+        char buff[64];
+        sprintf( (char*) buff,"1050,1500,1500,1500,0,0,0,0,0,0,0,2000\n");
+        if (!notconnected) {
+            RS232_SendBuf( (unsigned char*) buff, 63);
+            RS232_CloseComport();
+        }
+        usleep(100000);
+        notconnected = RS232_OpenComport(baudrate);
+
+        if (notconnected)
+            std::cout << "Bind failure" << std::endl;
+    }
+}
+
+
+void Arduino::close() {
+    exitSendThread = true;
+    g_lockData.unlock();
+    thread_nrf.join();
+
+}
---
+++ src/arduino.h
@@ -0,0 +1,45 @@
+#ifndef ARDUINO_H
+#define ARDUINO_H
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <math.h>
+#include <iomanip>
+#include <unistd.h>
+#include "common.h"
+
+#include "rs232.h"
+#include <thread>
+#include <mutex>
+
+class Arduino{
+
+public:
+
+    void init(bool fromfile);
+    void rebind(void);
+    void close();
+
+    int ledpower = 10;
+    uint16_t mode = 1500; // <min = mode 1, 1500 = mode 2, >max = mode 3
+    int roll,pitch,yaw = 1500;
+    int throttle = 1000;
+    int rebindValue = 0;
+
+    std::mutex g_lockData;
+
+private:
+
+    int baudrate;
+
+    std::thread thread_nrf;
+
+    bool exitSendThread = false;
+
+    void workerThread(void);
+    void sendData(void);
+
+};
+
+#endif // ARDUINO_H
