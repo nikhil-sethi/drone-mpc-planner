7f31181aec3ef1295ada34f552e044f7be3d17be
Kevindehecker <kevindehecker@hotmail.com>
2019-06-29 10:11:22 +0200

    Fix warning

--- src/dronenavigation.cpp
+++ src/dronenavigation.cpp
@@ -91,7 +91,7 @@ void DroneNavigation::update(float time) {
         _navigation_status = ns_wait_locate_drone;
         break;
     } case ns_wait_locate_drone: {
-        static float prev_time = time;
+        static float __attribute__((unused)) prev_time = time;
 #if TX_TYPE == TX_FRSKYD || TX_TYPE == TX_FRSKYX
         if (time - prev_time > 7 && time - prev_time < 8) {
             _dctrl->blink_drone(false); // refresh the blinking
