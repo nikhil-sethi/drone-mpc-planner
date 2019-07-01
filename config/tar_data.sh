d5b26ab723082939a3682a8f7e0ad8cebca8e1ba
kevindehecker <kevindehecker@hotmail.com>
2019-07-01 10:32:49 +0200

    Update tar_data.sh

--- config/tar_data.sh
+++ config/tar_data.sh
@@ -5,7 +5,7 @@ mkdir ~/data_tmp/
 cp ~/data/* ~/data_tmp/ -r
 cd ~/data_tmp/
 
-find -iname videoRawLR.avi -exec ffmpeg -i {} -c:v libx264 -level 3.0 -pix_fmt yuv420p -crf 21 -preset slow {}_reencoded.mp4 || true \; -exec rm {} \;
+find -iname videoRawLR.avi -exec ffmpeg -i {} -c:v libx264 -level 3.0 -pix_fmt yuv420p -crf 21 -preset slow {}_reencoded.mp4 \; -exec rm {} \;
 
 find -iname videoRawLR.avi -exec rm {} \;
 find -iname terminal.log -exec rm {} \;
