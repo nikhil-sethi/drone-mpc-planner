454f488fcf37a86b4335f13b3938c204838ba389
kevindehecker <kevindehecker@hotmail.com>
2019-08-07 13:59:14 +0200

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -27,7 +27,7 @@ fi
 	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 	sudo apt-get install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-	sudo snap install sublime-text
+	sudo snap install sublime-text --classic
 	sudo apt-get update
 	sudo apt install -y cmake g++ libva-dev libswresample-dev libavutil-dev pkg-config libcurl4-openssl-dev ncdu openssh-server ffmpeg -y 
 	sudo apt install  -y openssh-server gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav libgstreamer-plugins-base1.0-* libgstreamer-plugins-bad1.0-* libgstreamer-plugins-good1.0-* terminator
@@ -174,4 +174,5 @@ echo "***********************************************************"
 echo todo: 
 echo 1. Install qt creator and perform steps listed in https://github.com/pats-drones/pats/blob/master/config/qtcreator.md
 echo 2. Set bios to startup always at power on
+echo 3. Add phone wifi ssid
 echo "***********************************************************"
