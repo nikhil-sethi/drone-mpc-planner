d546a7dad0075118a063af96b0a9f1eaa942ba1f
kevindehecker <kevindehecker@hotmail.com>
2019-08-07 12:03:50 +0200

    Update install.sh

--- config/install.sh
+++ config/install.sh
@@ -21,7 +21,11 @@ fi
 
 # Install packages
 [ -f dependencies-packages.done ] || {
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+	sudo apt-get install apt-transport-https
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo snap install sublime-text
+	sudo apt-get update
 	sudo apt install -y cmake g++ libva-dev libswresample-dev libavutil-dev pkg-config libcurl4-openssl-dev sublime-text ncdu openssh-server ffmpeg -y 
 	sudo apt install  -y openssh-server gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav libgstreamer-plugins-base1.0-* libgstreamer-plugins-bad1.0-* libgstreamer-plugins-good1.0-* terminator
 	sudo apt-get remove -y modemmanager
@@ -163,7 +167,6 @@ set +x
 echo "***********************************************************"
 echo todo: 
 echo 1. Install qt creator and perform steps listed in https://github.com/pats-drones/pats/blob/master/config/qtcreator.md
-echo 2. Install sublime text
-echo 3. Set bios to startup always at power on
-echo 4. Install the ssh key files. (password access is disabled by default)
+echo 2. Set bios to startup always at power on
+echo 3. Install the ssh key files. (password access is disabled by default)
 echo "***********************************************************"
