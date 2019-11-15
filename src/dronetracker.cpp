73b6411eaa136b07fecfe5a7ef3593a1fa88a754
Kevin <kevindehecker@hotmail.com>
2019-11-15 23:37:06 +0100

    Faster detection of blob after takeoff

--- src/dronetracker.cpp
+++ src/dronetracker.cpp
@@ -162,11 +162,9 @@ ItemTracker::BlobWorldProps DroneTracker::calc_world_item(BlobProps * pbs, doubl
     } else if (taking_off() && wbp.valid && !_manual_flight_mode) {
 
         float dt = time - start_take_off_time;
-        if (dt < dparams.full_bat_and_throttle_spinup_duration) { // spin up time
-            wbp.valid = false;
-            return wbp;
-        }
         dt -= dparams.full_bat_and_throttle_spinup_duration;
+        if (dt<0)
+            dt=0;
 
         cv::Point3f expected_drone_location = _drone_blink_world_location;
         expected_drone_location.y+= 0.5f*dparams.full_bat_and_throttle_take_off_acc * powf(dt,2);
