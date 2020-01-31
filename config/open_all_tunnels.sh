51c646eaf83dd08c5c4faacc159b2a9ebf5b1857
kevindehecker <kevindehecker@hotmail.com>
2020-01-31 15:49:37 +0100

    Update open_all_tunnels.sh
    10 & 11

--- config/open_all_tunnels.sh
+++ config/open_all_tunnels.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh mavlab-gpu -L 6100:localhost:6100 -L 6200:localhost:6200 -L 6300:localhost:6300 -L 6400:localhost:6400 -L 6500:localhost:6500 -L 6600:localhost:6600 -L 6700:localhost:6700 -L 6800:localhost:6800 -L 6900:localhost:6900 
+ssh mavlab-gpu -L 6100:localhost:6100 -L 6200:localhost:6200 -L 6300:localhost:6300 -L 6400:localhost:6400 -L 6500:localhost:6500 -L 6600:localhost:6600 -L 6700:localhost:6700 -L 6800:localhost:6800 -L 6900:localhost:6900 -L 7000:localhost:7000 -L 7100:localhost:7100
