c1e4bf9db00c6a99b9749972dcae428e1edc82d6
Kevin <kevindehecker@hotmail.com>
2020-02-12 10:48:24 +0100

    Interactive classification

--- analysis/process_log.py
+++ analysis/process_log.py
@@ -1,156 +1,8 @@
 #!/usr/bin/env python3
-import csv
-import numpy as np
-import pandas as pd
 import os
-import pathlib
 from pathlib import Path
-import sys
-import random
-from shutil import copyfile
 import argparse
-
-def cp_videos_for_checking(source_folder,files,n,dest_folder,fn_prefix,no_video,reprocess):
-    fn_prefix = fn_prefix.replace('__','_') # this may happen during reprocess, because then the folder id is already in the name and sf = ''
-    pathlib.Path(dest_folder).mkdir(parents=True, exist_ok=True)
-
-    if reprocess:
-        n = len(files)
-
-    if n > len(files):
-        n = len(files)
-    if n > 0:
-        files = random.sample(files, n)
-
-        for fn_csv in files:
-
-            fn = fn_csv[0:len(fn_csv)-4]
-            if reprocess:
-                fn = fn.replace('no_moth_','')
-                fn = fn.replace('moth_','')
-            else:
-                fn = fn.replace('log','')
-            fn_new_csv = fn_prefix + fn + ".csv"
-
-            copyfile(Path(source_folder,fn_csv), Path(dest_folder,fn_new_csv))
-            if not reprocess:
-                header = ""
-                fn_header = "log.csv"
-                with open (Path(source_folder,fn_header), "r") as myfile:
-                    header=myfile.readlines()
-                line_prepender(Path(dest_folder,fn_new_csv),header[0])
-
-            if not no_video:
-                if reprocess:
-                    fn_mp4 = fn_csv[0:len(fn_csv)-4] + ".mp4"
-                else:
-                    fn_mp4 = fn_csv[0:len(fn_csv)-4] + ".mp4"
-                    fn_mp4 = "insect" + fn_mp4[3:len(fn_mp4)]
-                fn_new_mp4 = fn_prefix + fn + ".mp4"
-                if Path(source_folder,fn_mp4).is_file():
-                    copyfile(Path(source_folder,fn_mp4), Path(dest_folder,fn_new_mp4))
-
-def process_folder(source_folder, reprocess, verbose):
-    if not reprocess:
-        header_filename = Path(source_folder,"log.csv")
-        if not header_filename.is_file():
-            return list(), list(),list()
-        with open(header_filename) as header_file:
-            headers = csv.reader(header_file, delimiter=';')
-            headers = list(headers)
-            headers = headers[0]
-            headers = [x.strip(' ') for x in headers]
-
-    files = list_csv_files(source_folder)
-    files_moth = list()
-    files_no_moth = list()
-    files_corrupted = list()
-    i = 0
-    n = sum(1 for i in list_csv_files(source_folder)) #python=KUT
-    for f in files:
-        i = i + 1
-        printProgressBar(i,n)
-        source_csv_file = Path(source_folder,f)
-        try:
-            if check_log(source_csv_file,headers,verbose):
-                files_moth.append(f)
-            else:
-                files_no_moth.append(f)
-        except Exception:
-            files_corrupted.append(f)
-
-    return files_moth,files_no_moth,files_corrupted
-
-def check_log(source_csv_file,headers,verbose):
-    if os.stat(source_csv_file).st_size < 1000:
-        if verbose:
-            print("No moth because: file size too small")
-        return False
-    else:
-        with open(source_csv_file) as log_file:
-            if not headers:
-                log=pd.read_csv(log_file, sep=';',header=0)
-                log.rename(columns=lambda x: x.strip(), inplace=True)
-            else:
-                log=pd.read_csv(log_file, names=headers, sep=';',header=None)
-            
-        return pre_checks(log,verbose)
-
-                   
-def pre_checks(log,verbose):
-    if log.size < 5:
-        return False
-    else:
-        velx = log["svelX_insect"].astype(float)
-        if velx.abs().sum() < 0.1:
-            if verbose:
-                print("No moth because: velocity zero")
-            return False
-        else:
-            posz = log["sposZ_insect"].astype(float)
-            if posz.mean() < -1:
-                if verbose:
-                    print("No moth because: posZ < -1")
-                return False
-            else:
-                if verbose:
-                    print("MOTH DETECTED!")
-                return True
-
-def line_prepender(filename, line):
-    with open(filename, 'r+') as f:
-        content = f.read()
-        f.seek(0, 0)
-        f.write(line.rstrip('\r\n') + '\n' + content)
-
-from os import listdir
-def list_csv_files(directory):
-    return (f for f in listdir(directory) if f.endswith('.csv') and not f.startswith("log."))
-
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 75, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        print()
-
-
-
-
+from processloglib import *
 
 parser = argparse.ArgumentParser(description='Process and check the logs.')
 parser.add_argument('-s', '--source_folder',type=str,default="/home/pats/data/")
@@ -158,21 +10,18 @@ parser.add_argument('-d', '--dest_folder',type=str,default="/home/pats/data_chec
 parser.add_argument('-f', '--file',type=str,default="")
 parser.add_argument('-n', '--novideos', dest='novideo', action='store_true')
 parser.add_argument('-r', '--reprocess', dest='reprocess', action='store_true')
-
 parser.add_argument('-v', '--verbose', action='store_true')
-
 args = parser.parse_args()
-# parser.print_help()
 
 if args.file != "":
-    check_log(os.path.expanduser(args.file.strip(" ")),list(),True)
+    res, log = check_log(os.path.expanduser(args.file.strip(" ")),[],True)
 else:
     source_folder = os.path.expanduser(args.source_folder.strip(" "))
     dest_folder = os.path.expanduser(args.dest_folder.strip(" "))
     subfolders = [ name for name in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, name)) ]
 
     if args.reprocess:
-        subfolders.append("")
+        subfolders.append("") #the results from this script put all files in a single directory. so when reprocessing we don't have subfolders anymore
 
     i = 0
     all_files_corrupted = list()
---
+++ analysis/processloglib.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python3
+import csv
+import numpy as np
+import pandas as pd
+import os
+import pathlib
+from pathlib import Path
+import random
+from shutil import copyfile
+
+def cp_videos_for_checking(source_folder,files,n,dest_folder,fn_prefix,no_video,reprocess):
+    fn_prefix = fn_prefix.replace('__','_') # this may happen during reprocess, because then the folder id is already in the name and sf = ''
+    pathlib.Path(dest_folder).mkdir(parents=True, exist_ok=True)
+
+    if reprocess:
+        n = len(files)
+
+    if n > len(files):
+        n = len(files)
+    if n > 0:
+        files = random.sample(files, n)
+
+        for fn_csv in files:
+
+            fn = fn_csv[0:len(fn_csv)-4]
+            if reprocess:
+                fn = fn.replace('no_moth_','')
+                fn = fn.replace('moth_','')
+            else:
+                fn = fn.replace('log','')
+            fn_new_csv = fn_prefix + fn + ".csv"
+
+            copyfile(Path(source_folder,fn_csv), Path(dest_folder,fn_new_csv))
+            if not reprocess:
+                header = ""
+                fn_header = "log.csv"
+                with open (Path(source_folder,fn_header), "r") as myfile:
+                    header=myfile.readlines()
+                line_prepender(Path(dest_folder,fn_new_csv),header[0])
+
+            if not no_video:
+                if reprocess:
+                    fn_mp4 = fn_csv[0:len(fn_csv)-4] + ".mp4"
+                else:
+                    fn_mp4 = fn_csv[0:len(fn_csv)-4] + ".mp4"
+                    fn_mp4 = "insect" + fn_mp4[3:len(fn_mp4)]
+                fn_new_mp4 = fn_prefix + fn + ".mp4"
+                if Path(source_folder,fn_mp4).is_file():
+                    copyfile(Path(source_folder,fn_mp4), Path(dest_folder,fn_new_mp4))
+
+def process_folder(source_folder, reprocess, verbose):
+    if not reprocess:
+        header_filename = Path(source_folder,"log.csv")
+        if not header_filename.is_file():
+            return list(), list(),list()
+        with open(header_filename) as header_file:
+            headers = csv.reader(header_file, delimiter=';')
+            headers = list(headers)
+            headers = headers[0]
+            headers = [x.strip(' ') for x in headers]
+
+    files = list_csv_files(source_folder)
+    files_moth = list()
+    files_no_moth = list()
+    files_corrupted = list()
+    i = 0
+    n = sum(1 for i in list_csv_files(source_folder)) #python=KUT
+    for f in files:
+        i = i + 1
+        printProgressBar(i,n)
+        source_csv_file = Path(source_folder,f)
+        try:
+            if check_log(source_csv_file,headers,verbose):
+                files_moth.append(f)
+            else:
+                files_no_moth.append(f)
+        except Exception:
+            files_corrupted.append(f)
+
+    return files_moth,files_no_moth,files_corrupted
+
+
+def get_dataset(source_folder,n0,n1):
+    
+    files = list_csv_files(source_folder)
+    i = 0
+    n = sum(1 for i in list_csv_files(source_folder)) #python=KUT
+    
+    data = list()
+    for f in files:
+        i = i + 1
+        printProgressBar(i,n)
+        source_csv_file = Path(source_folder,f)
+        try:
+            pre_check_ok, log = read_log(source_csv_file,[],False)
+            if pre_check_ok:
+                imLx,imLy,RS_ID,posX,posY,posZ = get_chunk(log,n0,n1)
+                data.append(np.transpose([RS_ID,imLx,imLy,posX,posY,posZ]))
+        except Exception:
+            pass
+
+    return data
+
+def read_log(source_csv_file,headers,verbose):
+    if os.stat(source_csv_file).st_size < 1000:
+        if verbose:
+            print("No moth because: file size too small")
+        return False,[]
+    else:
+        with open(source_csv_file) as log_file:
+            if not headers:
+                log=pd.read_csv(log_file, sep=';',header=0)
+                log.rename(columns=lambda x: x.strip(), inplace=True)
+            else:
+                log=pd.read_csv(log_file, names=headers, sep=';',header=None)
+            
+            if len(log.index) < 5:
+                if verbose:
+                    print("Not enough rows")
+                return False,log
+
+        return True,log
+
+def check_log(source_csv_file,headers,verbose):
+    res,log = read_log(source_csv_file,headers,verbose)
+    if res:
+        return check_for_moths(log,verbose),log
+    else:
+        return False,log
+
+def check_for_moths(log,verbose):
+    velx = log["svelX_insect"].astype(float)
+    if velx.abs().sum() < 0.1:
+        if verbose:
+            print("No moth because: velocity zero")
+        return False
+    else:
+        posz = log["sposZ_insect"].astype(float)
+        if posz.mean() < -1:
+            if verbose:
+                print("No moth because: posZ < -1")
+            return False
+        else:
+            if verbose:
+                print("MOTH DETECTED!")
+            return True
+
+def get_chunk(log,n0,n1):
+    if len(log["imLx_insect"]) < n1:
+        n1 = len(log["imLx_insect"])
+    if n0 > n1:
+        n0 = n1
+    imLx_insect = np.array(log["imLx_insect"].astype(float))[n0:n1]
+    imLy_insect = np.array(log["imLy_insect"].astype(float))[n0:n1]
+    RS_ID = np.array(log["RS_ID"].astype(int))[n0:n1]
+
+    posX_insect = np.array(log["posX_insect"].astype(float))[n0:n1]
+    posY_insect = np.array(log["posY_insect"].astype(float))[n0:n1]
+    posZ_insect = np.array(log["posZ_insect"].astype(float))[n0:n1]
+     
+    return imLx_insect,imLy_insect,RS_ID,posX_insect,posY_insect,posZ_insect
+
+def line_prepender(filename, line):
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
+
+from os import listdir
+def list_csv_files(directory):
+    return (f for f in listdir(directory) if f.endswith('.csv') and not f.startswith("log."))
+
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 75, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
