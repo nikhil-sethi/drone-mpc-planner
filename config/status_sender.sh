1cd3cb0df8aabd855e365d41655fdba2095b0657
Kevin <kevindehecker@hotmail.com>
2020-02-24 22:27:33 +0100

    Wait until file exists

--- config/status_sender.sh
+++ config/status_sender.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-set -ex
+set -x
 
 LOCAL_STATUS_TXT_FILE="/home/$USER/pats_status.txt"
-LOCAL_STATUS_IM_FILE="~/home/$USER/pats_monitor_tmp.jpg"
+LOCAL_STATUS_IM_FILE="/home/$USER/pats_monitor_tmp.jpg"
 REMOTE_STATUS_TXT_FILE="status/${HOSTNAME}_status.txt"
 REMOTE_STATUS_IM_FILE="status/${HOSTNAME}_status.jpg"
 
+while [ ! -f "$LOCAL_STATUS_TXT_FILE" ]; do
+	sleep 10
+	echo "Warning: $LOCAL_STATUS_TXT_FILE does not exist"
+done
 while inotifywait -e modify,create, $LOCAL_STATUS_TXT_FILE; do
 	rsync -z $LOCAL_STATUS_TXT_FILE mavlab-gpu:$REMOTE_STATUS_TXT_FILE
 	if test -f "$LOCAL_STATUS_IM_FILE"; then
