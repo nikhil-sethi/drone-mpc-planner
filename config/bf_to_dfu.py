d0692c1b26197a96e34919e35a92fe5fc2027819
Kevin <kevindehecker@hotmail.com>
2020-03-27 16:57:31 +0100

    Fix bugs in upload script

--- config/bf_to_dfu.py
+++ config/bf_to_dfu.py
@@ -4,9 +4,7 @@ import serial
 
 ser = serial.Serial(port='/dev/ttyACM0')
 time.sleep(0.1)
-hoer = '#'
-ser.write(hoer.encode())
+ser.write('#'.encode())
 time.sleep(1)
-hoer = 'bl\n'
-ser.write(hoer.encode())
+ser.write('bl\n'.encode())
 
--- config/bf_upload_settings.py
+++ config/bf_upload_settings.py
@@ -3,18 +3,28 @@ import time
 import serial
 
 ser = serial.Serial(port='/dev/ttyACM0')
-time.sleep(0.1)
+# time.sleep(10.1)
+# print('hoer')
+# ser.write('save\n'.encode())
+# time.sleep(3.1)
+# print('hoer')
+# exit()
 
 ser.write('#'.encode())
-time.sleep(0.1)
+time.sleep(1)
 
 with open('BF_Trashcan.txt', 'r') as fp:
 	for cnt, line in enumerate(fp):
 		print("Line {}: {}".format(cnt, line))
 		ser.write(line.encode())
 		ser.write('\n'.encode())
-		time.sleep(0.01)
+		time.sleep(0.05)
 	ser.write('save\n'.encode())
+	time.sleep(0.5)
+	ser.close()
+ser = serial.Serial(port='/dev/ttyACM0')
+time.sleep(1)
+
 print("Uploading settings successfull")
 		
 
