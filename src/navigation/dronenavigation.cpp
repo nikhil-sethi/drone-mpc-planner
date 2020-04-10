nikhil-sethi/drone-mpc-planner

commit 30d664b198963ec8c1f7d5281dafbc8e0e3f468c

State-machine changed to reset heading close to camera

src/navigation/dronenavigation.cpp
@@ -246,6 +246,10 @@ void DroneNavigation::update(double time) {
                 _navigation_status = ns_flower_waypoint;
             else if(current_waypoint->mode == wfm_brick)
                 _navigation_status = ns_brick_waypoint;
+            else if (current_waypoint->mode == wfm_yaw_reset)
+                _navigation_status = ns_initial_reset_yaw;
+            else if (current_waypoint->mode == wfm_landing)
+                _navigation_status = ns_land;
             else
                 _navigation_status = ns_approach_waypoint;
             break;
@@ -311,21 +315,21 @@ void DroneNavigation::update(double time) {
                 _navigation_status=ns_manual;
             break;
         } case ns_initial_reset_yaw: {
+            setpoint_pos_world = current_waypoint->xyz;
             _dctrl->flight_mode(DroneController::fm_initial_reset_yaw);
-            if(time-time_initial_reset_yaw>0.5) {
+            float pos_error = normf( setpoint_pos_world - _trackers->dronetracker()->Last_track_data().pos());
+            if(time-time_initial_reset_yaw>0.5
+                && pos_error<0.3f){
                 _trackers->dronetracker()->detect_yaw();
                 _navigation_status = ns_wait_reset_yaw;
             }
             break;
         } case ns_wait_reset_yaw: {
             _dctrl->flight_mode(DroneController::fm_reset_yaw);
-            cv::Point3f err = _trackers->dronetracker()->Last_track_data().pos() - _trackers->dronetracker()->drone_landing_location();
-            err.y = 0;
-            float horizontal_err = normf(err);
-            if(_trackers->dronetracker()->check_smooth_yaw()
-                    && _trackers->dronetracker()->check_yaw()
-                    && horizontal_err < 0.3f) {
-                _navigation_status = ns_land;
+            if(_trackers->dronetracker()->check_yaw()
+                && _trackers->dronetracker()-> check_smooth_yaw()){
+                _navigation_status = ns_set_waypoint;
+                wpid++;
             }
             break;
         } case ns_land: {
@@ -382,8 +386,6 @@ void DroneNavigation::next_waypoint(Waypoint wp) {
 
     setpoint_vel_world = {0,0,0};
     setpoint_acc_world = {0,0,0};
-
-
 }
 
 void DroneNavigation::deserialize_settings() {

src/navigation/flightplan.h
@@ -17,7 +17,8 @@ static const char* waypoint_flight_modes_str[] = {
     "wfm_flower",
     "wfm_brick",
     "wfm_wp_stay",
-    "wfm_landing"
+    "wfm_landing",
+    "wfm_yaw_reset",
     "" // must be the last entry! (check in serializer)
 };
 
@@ -42,7 +43,7 @@ struct Waypoint_Landing : Waypoint {
 };
 struct Waypoint_Yaw_Reset : Waypoint{
     Waypoint_Yaw_Reset(){
-        xyz = cv::Point3f(0,.5f,0); // only for landing wp, relative to the startup location!
+        xyz = cv::Point3f(0.0f,-0.5f,-0.9f);
         threshold_mm = 50;
         mode = wfm_yaw_reset;
     }
@@ -132,6 +133,10 @@ public:
             break;
         } case waypoint_flight_modes::wfm_wp_stay: {
             Waypoint_Stay wp(cv::Point3f(x.value(),y.value(),z.value()));
+            return wp;
+            break;
+        } case waypoint_flight_modes::wfm_yaw_reset: {
+            Waypoint_Yaw_Reset wp;
             return wp;
             break;
         } case waypoint_flight_modes::wfm_brick: {

xml/flightplans/demo.xml
@@ -30,6 +30,13 @@
             <Member Name="z">-2.0</Member>
         </Class>
         <Class Type="Waypoint" Version="1.0">
+            <Member Name="type">wfm_yaw_reset</Member>
+            <Member Name="threshold_mm"></Member>
+            <Member Name="x"></Member>
+            <Member Name="y"></Member>
+            <Member Name="z"></Member>
+        </Class>
+        <Class Type="Waypoint" Version="1.0">
             <Member Name="type">wfm_landing</Member>
             <Member Name="threshold_mm"></Member>
             <Member Name="x"></Member>

commit 4743e5ee8bb383a9e82ab31e36c5c2165d9e13c9

Undo state machine change 30d664b198963ec8c1f7d5281dafbc8e0e3f468c, which caused the drone to do its headless mode disable before reaching the landing waypoint.

src/navigation/dronenavigation.cpp
@@ -263,10 +263,6 @@ void DroneNavigation::update(double time) {
                 _navigation_status = ns_flower_waypoint;
             else if(current_waypoint->mode == wfm_brick)
                 _navigation_status = ns_brick_waypoint;
-            else if (current_waypoint->mode == wfm_yaw_reset)
-                _navigation_status = ns_initial_reset_yaw;
-            else if (current_waypoint->mode == wfm_landing)
-                _navigation_status = ns_land;
             else
                 _navigation_status = ns_approach_waypoint;
             break;
@@ -285,10 +281,11 @@ void DroneNavigation::update(double time) {
                     && normf(_trackers->dronetracker()->Last_track_data().state.vel) < 1.6f
                     && _trackers->dronetracker()->n_frames_tracking>5)
             {
-                if (current_waypoint->mode == wfm_landing) {
+                if (current_waypoint->mode == wfm_landing)
                     _navigation_status = ns_land;
-                    time_initial_reset_yaw = time;
-                } else if (wpid < waypoints.size()) { // next waypoint in flight plan
+                else if (current_waypoint->mode == wfm_yaw_reset)
+                    _navigation_status = ns_initial_reset_yaw;
+                else if (wpid < waypoints.size()) { // next waypoint in flight plan
                     wpid++;
                     _navigation_status = ns_set_waypoint;
                 } else if (wpid == waypoints.size()) {