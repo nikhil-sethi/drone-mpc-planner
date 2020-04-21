066d9389e9a594c78a0015810e5bf28893e8d3f7
Kevin <kevindehecker@hotmail.com>
2020-04-21 16:18:33 +0200

    Install vscode on systems

--- config/install.sh
+++ config/install.sh
@@ -41,6 +41,7 @@ pushd ~/dependencies
 	sudo apt-get install apt-transport-https
 	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 	sudo snap install sublime-text --classic
+    sudo snap install code --classic
 	sudo apt-get update
 	sudo apt install -y cmake g++ libva-dev libswresample-dev libavutil-dev pkg-config libcurl4-openssl-dev ncdu openssh-server ffmpeg astyle unattended-upgrades inotify-tools cpputest
 	sudo apt install  -y openssh-server gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav libgstreamer-plugins-base1.0-* libgstreamer-plugins-bad1.0-* libgstreamer-plugins-good1.0-* gstreamer1.0-vaapi vainfo meld gitk git-gui terminator
