36dba47308c09f9e51d5b9c7b792d79a5b423e85
Kevin <kevindehecker@hotmail.com>
2020-05-27 18:11:46 +0200

    [cc] update the branch to the branch currently used on the command center pc

--- commandcenter/update_system.sh
+++ commandcenter/update_system.sh
@@ -3,12 +3,13 @@ set -e
 
 echo "Update $1"
 count=0
+branch=$(git rev-parse --abbrev-ref HEAD)
 until (( count++ >= 5 )) || ssh -o StrictHostKeyChecking=no -T $1 << EOF
  killall pats || true
  cd code/pats/pc/build
  git fetch
  git reset --hard
- git checkout deploy
+ git checkout $branch
  git pr
  cmake ..
  make -j4
