12dd081f0d78e439e72ce29e8a24c4ebd078a6ef
Kevin <kevindehecker@hotmail.com>
2020-07-01 17:22:01 +0200

    Tuning betaflight 4.1.5 Hammer @koppert

--- config/firmwares/drone/bf_flash.py
+++ config/firmwares/drone/bf_flash.py
@@ -51,7 +51,7 @@ def upload_settings(port,drone_id,settings):
 
 parser = argparse.ArgumentParser(description='Process and check the logs.')
 parser.add_argument('-i', '--drone-id',type=str,default='1')
-parser.add_argument('-f', '--firmware',help='Firmware *.bin file path',type=str,default='trashcan_4.1.5.bin')
+parser.add_argument('-f', '--firmware',help='Firmware *.bin file path',type=str,default='pats_4.1.5.bin')
 parser.add_argument('-s', '--settings',help='Betaflight diff settings file path', type=str,default='BF_Hammer_4-1-5_180.txt')
 parser.add_argument('-p', '--port',type=str,default='/dev/ttyACM0')
 parser.add_argument('-n', '--no-flashing', action='store_true')
