5ffe6f8b39bbafcb8a29e363940525f3c777e569
Kevin <kevindehecker@hotmail.com>
2020-07-10 00:56:11 +0200

    [cc] Fix search script connection issues

--- commandcenter/search_in_system.sh
+++ commandcenter/search_in_system.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
-set -e
 
 echo "System: $1"
-ssh -o StrictHostKeyChecking=no -T $1 << EOF
+count=0
+until (( count++ >= 5 )) || ssh -o StrictHostKeyChecking=no -T $1 << EOF
 	cd ~/data/
 	grep -irl "$2" ./$3*/terminal.log
 EOF
+do
+  sleep 1
+done
+if [ "${count}" -eq 6 ]; then
+	echo "Could not reach system :("
+fi
 echo "................."
