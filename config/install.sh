4e6286cd9cfefdd79b6ba88bff829bd2d2feee24
Kevin <kevindehecker@hotmail.com>
2020-07-13 16:09:12 +0200

    Config stuff for flight performance

--- config/install.sh
+++ config/install.sh
@@ -50,7 +50,7 @@ mkdir -p ~/code
 pushd ~/dependencies
 
 # Install pats dependency packages
-[ -f dependencies-packages-v1.1.done ] || {
+[ -f dependencies-packages-v1.2.done ] || {
 	sudo apt-get update  
 	sudo apt install -y cmake g++ libva-dev libswresample-dev libavutil-dev pkg-config libcurl4-openssl-dev ncdu openssh-server ffmpeg unattended-upgrades inotify-tools cpputest python3-pip python-pip dfu-util 
 	sudo apt install -y gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav libgstreamer-plugins-base1.0-* libgstreamer-plugins-bad1.0-* libgstreamer-plugins-good1.0-* gstreamer1.0-vaapi vainfo
@@ -58,7 +58,8 @@ pushd ~/dependencies
 	pip3 install pyserial
 	#specific to enable opencv features and optimizations:
 	sudo apt install -y yasm gfortran libjpeg8-dev libpng-dev libtiff-dev libatlas-base-dev libprotobuf-dev protobuf-compiler libgoogle-glog-dev libgflags-dev libgphoto2-dev libeigen3-dev libhdf5-dev libatlas3-base libatlas-base-dev liblapack3 liblapacke liblapacke-dev liblapack-dev ccache
-	touch dependencies-packages-v1.1.done
+	pip3 install numpy pandas
+	touch dependencies-packages-v1.2.done
 }
 
 # Add librealsense repository
