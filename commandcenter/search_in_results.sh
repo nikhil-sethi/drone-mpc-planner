0deb2004f67f191fb3575dd9b4457265513b5460
Kevin <kevindehecker@hotmail.com>
2020-09-21 14:40:33 +0200

    Search and list interception results

---
+++ commandcenter/search_in_results.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+echo "System: $1"
+count=0
+until (( count++ >= 5 )) || ssh -o StrictHostKeyChecking=no -T $1 << EOF
+	cd ~/data/
+	grep -ir "$2" ./$3*/logging/results.txt
+EOF
+do
+  sleep 1
+done
+if [ "${count}" -eq 6 ]; then
+	echo "Could not reach system :("
+fi
+echo "................."
+
+
+#grep -ir "best_interception_distance:0" ./20200919*
