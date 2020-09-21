f81f7050a943c1d01f8a55f4331e7795aeaf3231
kevindehecker <kevindehecker@hotmail.com>
2020-09-21 11:26:26 +0200

    Update search_in_system.sh
    Remove json generation from search script. Fix #473

--- commandcenter/search_in_system.sh
+++ commandcenter/search_in_system.sh
@@ -4,11 +4,7 @@ echo "System: $1"
 count=0
 until (( count++ >= 5 )) || ssh -o StrictHostKeyChecking=no -T $1 << EOF
 	cd ~/data/
-	grep -irl "$2" ./$3*/terminal.log > vids.txt
-	cat vids.txt
-	mkdir -p "$1"
-	python3 ~/code/pats/commandcenter/moth_cutter_search.py -i ~/data/ -o ~/data/"$1" -t vids.txt
-	
+	grep -irl "$2" ./$3*/terminal.log
 EOF
 do
   sleep 1
