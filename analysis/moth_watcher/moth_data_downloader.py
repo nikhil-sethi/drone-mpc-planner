28ff6d4c5f9efc0c7fc090c9c4594fb764c1c355
Kevin <kevindehecker@hotmail.com>
2020-09-26 13:56:48 +0200

    Auto download renders to moth server

--- analysis/moth_watcher/moth_data_downloader.py
+++ analysis/moth_watcher/moth_data_downloader.py
@@ -3,21 +3,42 @@ import os,subprocess,time
 
 source_folder = '~/Downloads/moth_jsons'
 
-def download(wait=False):
+def execute(cmd):
+    popen = subprocess.Popen(cmd, shell=True,stderr=subprocess.STDOUT,stdout=subprocess.PIPE)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        if popen.poll() != None:
+            break
+        print(stdout_line.decode('utf-8'),end ='')
+    popen.stdout.close()
+    return
+
+def download_jsons():
     global source_folder
     rsync_src='mavlab-gpu:/home/pats/moth_json/'
     subprocess.call(['mkdir -p ' + source_folder ], shell=True)
     cmd = ['rsync -zva --timeout=3 --exclude \'*.jpg.*\' --exclude \'*.xml.*\' --exclude \'*.txt.*\' ' + rsync_src + ' '+ source_folder]
-    if wait:
-        subprocess.call(cmd, shell=True,stdout=subprocess.PIPE)
-    else:
-        subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
+    execute(cmd)
 
+def download_renders(pats_id):
+    cmd = ['rsync -azvP pats' + str(pats_id) + ':data_rendered/* ~/Downloads/pats_renders']
+    execute(cmd)
+def download_renders_all():
+    download_renders(11)
+    download_renders(12)
+    download_renders(13)
+    download_renders(14)
+    download_renders(15)
+    download_renders(16)
+    download_renders(17)
+    download_renders(18)
+    download_renders(19)
+    download_renders(21)
 
 while True:
-    download()
-    print ('Downloaded')
+    download_jsons()
+    print ('Downloaded jsons')
     cmd = ['~/code/pats/analysis/moth_watcher/fill_db_with_jsons.py -i ' + source_folder]
     result = subprocess.run(cmd, shell=True,stdout=subprocess.PIPE)
     print(result.returncode, result.stdout, result.stderr)
+    download_renders_all()
     time.sleep(3600)
