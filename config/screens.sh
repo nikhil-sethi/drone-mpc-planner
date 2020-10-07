409a6d65c1c6a50b95de955540edd9ab64b4a989
Kevin <kevindehecker@hotmail.com>
2020-10-07 13:48:19 +0200

    [admin] make tunnel description shorter

--- config/screens.sh
+++ config/screens.sh
@@ -7,12 +7,12 @@ tunnels_port=( 22 22 16666)
 # Start tunnels
 for i in ${!tunnels[@]}
 do
-	/usr/bin/screen -t 'tunnel'$i $i /bin/bash -c '~/code/pats/config/tunnel.sh '${tunnels[i]}' '${tunnels_port[i]}'; exec /bin/bash'
+	/usr/bin/screen -t 't'$i $i /bin/bash -c '~/code/pats/config/tunnel.sh '${tunnels[i]}' '${tunnels_port[i]}'; exec /bin/bash'
 done
 
 # Start other screens
 i=$(($i + 1))
-/usr/bin/screen -t staupd $i /bin/bash -c 'cd ~/code/pats/config && ~/code/pats/config/status_sender.py; exec /bin/bash'
+/usr/bin/screen -t upl $i /bin/bash -c 'cd ~/code/pats/config && ~/code/pats/config/status_sender.py; exec /bin/bash'
 i=$(($i + 1))
 /usr/bin/screen -t pats $i /bin/bash -c 'cd ~/code/pats/config && ~/code/pats/config/autostart.sh 2>&1 | /usr/bin/tee -a ~/pats_daemon.log; exec /bin/bash'
 i=$(($i + 1))
