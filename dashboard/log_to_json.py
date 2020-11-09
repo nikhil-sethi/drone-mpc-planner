nikhil-sethi/drone-mpc-planner

commit f079025ef010517fc50e3cddd7ee02eff595726c

[analysis] Bugfixes: #550 dist_traj sqrt problem

dashboard/log_to_json.py
@@ -6,7 +6,7 @@ pd.options.mode.chained_assignment = None
 from tqdm import tqdm
 from pathlib import Path
 
-version = "1.6"
+version = "1.7"
 
 from scipy.interpolate import interp1d
 from sklearn.base import BaseEstimator, TransformerMixin
@@ -192,7 +192,7 @@ def process_detections_in_folder(folder,operational_log_start,mode):
         pos_end =  [x_tracking[-1],y_tracking[-1],z_tracking[-1]]
         diff = np.array(pos_end) - np.array(pos_start)
         dist_traveled = math.sqrt(np.sum(diff*diff))
-        dist_traject = math.sqrt(np.sum((x_tracking[1:] - x_tracking[:-1])**2 + (y_tracking[1:] - y_tracking[:-1])**2 + (z_tracking[1:] - z_tracking[:-1])**2))
+        dist_traject = np.sum(math.sqrt((x_tracking[1:] - x_tracking[:-1])**2 + (y_tracking[1:] - y_tracking[:-1])**2 + (z_tracking[1:] - z_tracking[:-1])**2))
 
         mid_id = int(round(len(x_tracking)/2))
         pos_middle =  [x_tracking[mid_id],y_tracking[mid_id],z_tracking[mid_id]]

commit 74ce543f10d916c4f3013fc74601e9cf135197c5

Bugfix np.sqrt instead of math.sqrt. Regression from f079025ef010517fc50e3cddd7ee02eff595726c

dashboard/log_to_json.py
@@ -192,7 +192,7 @@ def process_detections_in_folder(folder,operational_log_start,mode):
         pos_end =  [x_tracking[-1],y_tracking[-1],z_tracking[-1]]
         diff = np.array(pos_end) - np.array(pos_start)
         dist_traveled = math.sqrt(np.sum(diff*diff))
-        dist_traject = np.sum(math.sqrt((x_tracking[1:] - x_tracking[:-1])**2 + (y_tracking[1:] - y_tracking[:-1])**2 + (z_tracking[1:] - z_tracking[:-1])**2))
+        dist_traject = np.sum(np.sqrt((x_tracking[1:] - x_tracking[:-1])**2 + (y_tracking[1:] - y_tracking[:-1])**2 + (z_tracking[1:] - z_tracking[:-1])**2))
 
         mid_id = int(round(len(x_tracking)/2))
         pos_middle =  [x_tracking[mid_id],y_tracking[mid_id],z_tracking[mid_id]]