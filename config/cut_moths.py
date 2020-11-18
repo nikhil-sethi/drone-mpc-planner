ace5943bfe6065c5ac8a75bb9bce98eb9e154ff0
Kevin <kevindehecker@hotmail.com>
2020-11-18 11:19:11 +0100

    [C] #188: don't cut false positives

--- config/cut_moths.py
+++ config/cut_moths.py
@@ -14,7 +14,6 @@ def execute(cmd):
     popen.stdout.close()
 
 def system_was_monitoring_in_folder(folder):
-    pats_xml_mode = ''
     pats_xml_path = Path(folder,'logging','pats.xml')
     if not os.path.exists(pats_xml_path):
         print("Error: pats.xml not found in: " + folder)
@@ -36,7 +35,6 @@ def natural_sort(l):
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     return sorted(l, key=alphanum_key)
 
-
 parser = argparse.ArgumentParser(description='Script that cuts the raw video into moths based on the log')
 parser.add_argument('-i', help="Path to the folder", required=True)
 args = parser.parse_args()
@@ -48,9 +46,6 @@ if not os.path.exists(frames_fn):
     print("Frames.csv not found")
     exit()
 with open(frames_fn, 'r') as flog:
-
-
-
     if system_was_monitoring_in_folder(folder):
         files = tqdm(natural_sort([fp for fp in glob.glob(os.path.join(folder, "logging", "log_itrk*.csv")) if "itrk0" not in fp]))
         with open(cut_log_fn, 'w') as clog:
@@ -65,44 +60,46 @@ with open(frames_fn, 'r') as flog:
                         lines = ilog.read().splitlines()
                         fn = os.path.basename(file)
                         file_id = fn.split('.')[0][8:]
-                        if len(lines) > 40: # only cut a video if it is of substantial length...
-                            video_start_rs_id = int(lines[1].split(';')[0])
-                            while True:
-                                fline = flog.readline()
-                                video_start_time =-1
-                                video_start_video_id = -1
-                                if not fline or len(fline.split(';')) !=4:
-                                    clog.write('Error: could not find frame in frames.csv\n')
-                                    print('Error: could not find frame in frames.csv')
-                                    break
-                                if int(fline.split(';')[2]) == video_start_rs_id:
-                                    video_start_video_id = int(fline.split(';')[1])
-                                    video_start_time = float(video_start_video_id)/90
-                                    break
-                                if int(fline.split(';')[2]) > video_start_rs_id:
-                                    clog.write('Warning: frame id already passed. Skipping this one because lazy.\n')
-                                    print('Warning: frame id already passed. Skipping this one because lazy.')
-                                    break
+                        if len(lines) > 10: # only cut a video if it is of substantial length...
+                            heads = lines[0].split(';')
+                            fp = lines[-1].split(';')[heads.index('fp')]
+                            if fp == 'fp_not_a_fp':
+                                video_start_rs_id = int(lines[1].split(';')[0])
+                                while True:
+                                    fline = flog.readline()
+                                    video_start_time =-1
+                                    video_start_video_id = -1
+                                    if not fline or len(fline.split(';')) !=4:
+                                        clog.write('Error: could not find frame in frames.csv\n')
+                                        print('Error: could not find frame in frames.csv')
+                                        break
+                                    if int(fline.split(';')[2]) == video_start_rs_id:
+                                        video_start_video_id = int(fline.split(';')[1])
+                                        video_start_time = float(video_start_video_id)/90
+                                        break
+                                    if int(fline.split(';')[2]) > video_start_rs_id:
+                                        clog.write('Warning: frame id already passed. Skipping this one because lazy.\n')
+                                        print('Warning: frame id already passed. Skipping this one because lazy.')
+                                        break
 
-                            if video_start_time < 0:
-                                continue
-                            video_start_time = video_start_time - 1
-                            if (video_start_time<0):
-                                video_start_time = 0
-                            video_end_rs_id = int(lines[-2].split(';')[0])
-                            video_end_video_id = video_end_rs_id - (video_start_rs_id-video_start_video_id)
-                            video_end_time = float(video_end_video_id)/90 + 1
-                            video_duration = video_end_time - video_start_time
-                            # print (str(file_id) + ': from ' + str(round(video_start_time,1)) + ' to ' + str(round(video_end_time,1)))
-                            log_folder = folder + '/logging/'
-
-                            video_out_file = log_folder + '/moth' + file_id + '.mkv'
-
-                            video_out_file = video_out_file.replace(os.path.expanduser('~'), '~')
-                            video_out_file = video_out_file.replace('//', '/')
-                            cmd = ' -c:v copy -an -ss ' + str(round(video_start_time,1)) + ' -t ' + str(round(video_duration,1)) + ' ' + video_out_file
-                            ffmpeg_cmd += cmd
-                            clog.write(cmd + '\n' )
+                                if video_start_time < 0:
+                                    continue
+                                video_start_time = video_start_time - 1
+                                if (video_start_time<0):
+                                    video_start_time = 0
+                                video_end_rs_id = int(lines[-2].split(';')[0])
+                                video_end_video_id = video_end_rs_id - (video_start_rs_id-video_start_video_id)
+                                video_end_time = float(video_end_video_id)/90 + 1
+                                video_duration = video_end_time - video_start_time
+                                log_folder = folder + '/logging/'
+                                video_out_file = log_folder + '/moth' + file_id + '.mkv'
+                                video_out_file = video_out_file.replace(os.path.expanduser('~'), '~')
+                                video_out_file = video_out_file.replace('//', '/')
+                                cmd = ' -c:v copy -an -ss ' + str(round(video_start_time,1)) + ' -t ' + str(round(video_duration,1)) + ' ' + video_out_file
+                                ffmpeg_cmd += cmd
+                                clog.write(cmd + '\n' )
+                            else:
+                                clog.write(fp + '\n')
                         else:
                             clog.write('Not enough lines!\n')
 
