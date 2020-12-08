nikhil-sethi/drone-mpc-planner

commit 6b1c52461aaee2c101085bdafec586029ea55624

[CV] Improve tracking sensitivity and reliability

src/vizs.cpp
@@ -405,7 +405,6 @@ cv::Mat Visualizer::draw_sub_tracking_viz(cv::Mat frameL_small, cv::Size vizsize
     return frameL_small_drone;
 }
 
-
 void Visualizer::update_tracker_data(cv::Mat frameL, cv::Point3f setpoint, double time, bool draw_plots) {
     enable_plots = draw_plots;
     if (new_tracker_viz_data_requested) {
@@ -423,6 +422,10 @@ void Visualizer::update_tracker_data(cv::Mat frameL, cv::Point3f setpoint, doubl
 
             if (dis < min_dis)
                 min_dis = dis;
+        }
+
+        if (dtrkr->takeoff_location_valid()) {
+            ground_y = dtrkr->takeoff_location().y;
         }
 
         tracker_viz_base_data.frameL = frameL;
@@ -475,31 +478,33 @@ void Visualizer::draw_tracker_viz() {
         cv::circle(frameL_color,p.pt()*pparams.imscalef,p.size/2*pparams.imscalef,cv::Scalar(0,0,255));
     }
 
-    if (last_insect_detection.predicted_image_item.valid) {
-        auto pred = last_insect_detection.predicted_image_item;
-        cv::circle(frameL_color,pred.pt(),pred.size/2,cv::Scalar(0,255,0));
-    }
-
-    if ( last_insect_detection.world_item.iti.valid) {
+    cv::Scalar itrkr_color = cv::Scalar(0,0,128);
+    if ( last_insect_detection.predicted_image_item.valid) {
         std::stringstream ss;
         tracking::WorldItem wti = last_insect_detection.world_item;
+
+        cv::Point2i p_ins_im = last_insect_detection.predicted_image_item.pt();
+        float ins_size = last_insect_detection.predicted_image_item.size;
+        ss << "i ";
         if (wti.valid) {
-            ss << "i " << to_string_with_precision(wti.distance,1);
-            cv::Scalar c(0,0,255);
-            if (wti.distance_bkg >wti.distance )
-                c = cv::Scalar(0,180,255); //first number cannot be 0 because of super weird qtcreator / gdb bug
-            cv::Point2i p_ins_im  = wti.iti.pt() *pparams.imscalef;
-            putText(frameL_color,ss.str(),p_ins_im,cv::FONT_HERSHEY_SIMPLEX,0.5,c);
-            cv::line(frameL_color,p_ins_im,p_ins_im,c,2);
-            cv::Point3f p_ins_ground_wrld = wti.pt;
-            p_ins_ground_wrld.y = -1.92f; // guestimated ground level
-            cv::Point2f p_ins_ground_im = world2im_2d(p_ins_ground_wrld,_visdat->Qfi,_visdat->camera_angle);
-            cv::line(frameL_color,p_ins_im,p_ins_ground_im,white,1);
-            float ins_size = wti.iti.size;
-            if (ins_size <= 0) //replay insects don't have a proper size
-                ins_size = 2;
-            cv::circle(frameL_color,p_ins_im,ins_size*pparams.imscalef / 2.f,c);
+            ss << to_string_with_precision(wti.distance,1);
+            itrkr_color = cv::Scalar(0,0,255);
+            p_ins_im  = wti.iti.pt() * pparams.imscalef;
+            ins_size = wti.iti.size * pparams.imscalef;
         }
+        if (ins_size <= 0) //replay insects don't have a proper size
+            ins_size = 2;
+
+        putText(frameL_color,ss.str(),p_ins_im,cv::FONT_HERSHEY_SIMPLEX,0.5,itrkr_color);
+        cv::line(frameL_color,p_ins_im,p_ins_im,itrkr_color,2);
+        cv::Point3f p_ins_ground_wrld = wti.pt;
+        p_ins_ground_wrld.y = ground_y;
+        cv::Point2f p_ins_ground_im = world2im_2d(p_ins_ground_wrld,_visdat->Qfi,_visdat->camera_angle);
+        cv::line(frameL_color,p_ins_im,p_ins_ground_im,itrkr_color,1);
+
+
+        cv::circle(frameL_color,p_ins_im,ins_size*pparams.imscalef / 2.f,itrkr_color);
+
     }
 
     if (_dctrl->ff_interception()) {

src/dronecontroller.cpp
@@ -119,10 +119,10 @@ void DroneController::control(TrackData data_drone, TrackData data_target_new, T
 
     if (_joy_state== js_waypoint)
         data_raw_insect = data_target_new; // the takeoff burn uses raw insect, but wp flight mode also uses takeoff burn
-    _dtrk->update_drone_target(data_raw_insect.pos());
+    _dtrk->update_target(data_raw_insect.pos());
 
     if (!data_raw_insect.pos_valid) // if tracking is lost, set the raw setpoint to just above takeoff location, because otherwise the (takeoff) may go towards 0,0,0 (the camera)
-        data_raw_insect.state.pos = _dtrk->drone_takeoff_location() + cv::Point3f(0,0.5,0);
+        data_raw_insect.state.pos = _dtrk->takeoff_location() + cv::Point3f(0,0.5,0);
 
     _dist_to_setpoint = normf(data_drone.state.pos - data_target_new.state.pos); // TODO: [k] update this with raw data in burn mode???
 
@@ -178,7 +178,7 @@ void DroneController::control(TrackData data_drone, TrackData data_target_new, T
         [[fallthrough]];
     } case fm_take_off_aim: {
         StateData state_drone_takeoff = data_drone.state;
-        state_drone_takeoff.pos = _dtrk->drone_takeoff_location() + cv::Point3f(0,lift_off_dist_take_off_aim,0);
+        state_drone_takeoff.pos = _dtrk->takeoff_location() + cv::Point3f(0,lift_off_dist_take_off_aim,0);
         state_drone_takeoff.vel = {0};
 
         float remaing_spinup_time = dparams.full_bat_and_throttle_spinup_duration - aim_duration - time_spent_spinning_up(take_off_start_time);
@@ -329,9 +329,9 @@ void DroneController::control(TrackData data_drone, TrackData data_target_new, T
 
         model_error = 0;
         if(!data_drone.pos_valid && _time-take_off_start_time < 0.5) {
-            pos_modelx.internal_states(_dtrk->drone_takeoff_location().x, _dtrk->drone_takeoff_location().x);
-            pos_modely.internal_states(_dtrk->drone_takeoff_location().y, _dtrk->drone_takeoff_location().y);
-            pos_modelz.internal_states(_dtrk->drone_takeoff_location().z, _dtrk->drone_takeoff_location().z);
+            pos_modelx.internal_states(_dtrk->takeoff_location().x, _dtrk->takeoff_location().x);
+            pos_modely.internal_states(_dtrk->takeoff_location().y, _dtrk->takeoff_location().y);
+            pos_modelz.internal_states(_dtrk->takeoff_location().z, _dtrk->takeoff_location().z);
 
         } else {
             pos_modelx.internal_states(data_drone.pos().x, data_drone.pos().x);
@@ -356,7 +356,7 @@ void DroneController::control(TrackData data_drone, TrackData data_target_new, T
         if(!data_drone.pos_valid) {
             pos_err_i = {0,0,0};
             if(_time-take_off_start_time < 0.5)
-                data_drone.state.pos = _dtrk->drone_takeoff_location ();
+                data_drone.state.pos = _dtrk->takeoff_location ();
         }
 
         control_model_based(data_drone, data_target_new.pos(),data_target_new.vel());
@@ -369,7 +369,7 @@ void DroneController::control(TrackData data_drone, TrackData data_target_new, T
         if(!data_drone.pos_valid) {
             pos_err_i = {0,0,0};
             if(_time-take_off_start_time < 0.5)
-                data_drone.state.pos = _dtrk->drone_takeoff_location ();
+                data_drone.state.pos = _dtrk->takeoff_location ();
         }
         control_model_based(data_drone, data_target_new.pos(),data_target_new.vel());
         break;
@@ -746,7 +746,7 @@ std::tuple<float,float> DroneController::acc_to_quaternion(cv::Point3f acc) {
 //considering a take off order from just after the aiming phase of: spinning up, burning, spin down, 1g, find the max drone acceleration that best desccribes the current position given dt
 void DroneController::approx_effective_thrust(TrackData data_drone, cv::Point3f burn_direction, float burn_duration, float dt_burn) {
 
-    cv::Point3f pos_after_aim =  _dtrk->drone_takeoff_location() + cv::Point3f(0,lift_off_dist_take_off_aim,0);
+    cv::Point3f pos_after_aim =  _dtrk->takeoff_location() + cv::Point3f(0,lift_off_dist_take_off_aim,0);
     float partial_effective_burn_spin_up_duration = effective_burn_spin_down_duration; // if the burn duration is long enough this is equal otherwise it may be shortened
     cv::Point3f acc = burn_direction * drone_calibration.thrust ; // initial guess, variable to be optimized
     cv::Point3f meas_pos =  data_drone.pos(); // current measured drone position

src/dronecontroller.h
@@ -525,21 +525,28 @@ public:
     bool joystick_ready();
 
     void LED(bool b) {
-        _rc->LED_drone(b,dparams.drone_led_strength);
+        if (initialized)
+            _rc->LED_drone(b,dparams.drone_led_strength);
     }
     void LED(bool b, int value) {
-        _rc->LED_drone(b,value);
+        if (initialized)
+            _rc->LED_drone(b,value);
     }
     void stop_rc() {
-        _rc->close();
+        if (initialized)
+            _rc->close();
     }
-
     void beep(bool b) {
-        _rc->beep(b);
+        if (initialized)
+            _rc->beep(b);
     }
-
-    telemetry_data telemetry() {
-        return _rc->telemetry;
+    Telemetry telemetry() {
+        if (initialized)
+            return _rc->telemetry;
+        else {
+            Telemetry t;
+            return t;
+        }
     }
 
     void start_landing_acc_calibration() {

src/main.cpp
@@ -442,7 +442,7 @@ void process_frame(Stereo_Frame_Data data) {
     auto profile_t2_trkrs = std::chrono::high_resolution_clock::now();
 #endif
 
-    if (log_replay_mode) {
+    if (log_replay_mode && pparams.op_mode != op_mode_monitoring) {
         dctrl.insert_log(logreader.current_entry.joyRoll, logreader.current_entry.joyPitch, logreader.current_entry.joyYaw, logreader.current_entry.joyThrottle,logreader.current_entry.joyArmSwitch,logreader.current_entry.joyModeSwitch,logreader.current_entry.joyTakeoffSwitch,logreader.current_entry.auto_roll,logreader.current_entry.auto_pitch,logreader.current_entry.auto_throttle, logreader.current_entry.telem_acc_z, logreader.current_entry.telem_throttle, logreader.current_entry.telem_throttle_s, logreader.current_entry.maxthrust, logreader.current_entry.telem_thrust_rpm);
     }
     dnav.update(data.time);
@@ -450,7 +450,8 @@ void process_frame(Stereo_Frame_Data data) {
     auto profile_t3_nav = std::chrono::high_resolution_clock::now();
 #endif
 
-    dctrl.control(trackers.dronetracker()->last_track_data(),dnav.setpoint(),trackers.target_last_trackdata(),data.time);
+    if (pparams.op_mode != op_mode_monitoring)
+        dctrl.control(trackers.dronetracker()->last_track_data(),dnav.setpoint(),trackers.target_last_trackdata(),data.time);
 #ifdef PROFILING
     auto profile_t4_ctrl = std::chrono::high_resolution_clock::now();
 #endif
@@ -821,7 +822,8 @@ void init() {
     visdat.init(cam->Qf, cam->frameL,cam->frameR,cam->camera_angle(),cam->measured_gain(),cam->measured_exposure(),cam->depth_background_mm); // do after cam update to populate frames
     trackers.init(&logger,replay_dir, &visdat, &(cam->camera_volume));
     dnav.init(&logger,&trackers,&dctrl,&visdat, &(cam->camera_volume),replay_dir);
-    dctrl.init(&logger,replay_dir,generator_mode,&rc,trackers.dronetracker(), &(cam->camera_volume),cam->measured_exposure());
+    if (pparams.op_mode != op_mode_monitoring)
+        dctrl.init(&logger,replay_dir,generator_mode,&rc,trackers.dronetracker(), &(cam->camera_volume),cam->measured_exposure());
 
     if (render_monitor_video_mode)
         dnav.render_now_override();

src/multimodule.h
@@ -135,7 +135,7 @@ enum arming_states {
     ARMING_DISABLED_ARM_SWITCH      = (1 << 25)
 };
 
-struct telemetry_data {
+struct Telemetry {
     float           batt_v;
     float           batt_cell_v;
     float           batt_current;
@@ -161,7 +161,7 @@ public:
     int throttle = RC_BOUND_MIN;
     int arm_switch = RC_BOUND_MIN;
     int turtle_mode = RC_BOUND_MIN;
-    telemetry_data telemetry;
+    Telemetry telemetry;
     const int bf_major_required = 4;
     const int bf_minor_required = 2;
     const int bf_patch_required = 102;

src/navigation/dronenavigation.cpp
@@ -97,7 +97,7 @@ void DroneNavigation::update(double time) {
                 if (pparams.op_mode==op_mode_monitoring) {
                     _dctrl->flight_mode(DroneController::fm_disarmed);
                     _visdat->enable_background_motion_map_calibration(motion_calibration_duration);
-                    _visdat->create_overexposed_removal_mask(_trackers->dronetracker()->drone_takeoff_im_location(),_trackers->dronetracker()->drone_takeoff_im_size());
+                    _visdat->create_overexposed_removal_mask(_trackers->dronetracker()->takeoff_im_location(),_trackers->dronetracker()->takeoff_im_size());
                     time_motion_calibration_started = time;
                     _navigation_status = ns_calibrating_motion;
                     _dctrl->LED(false);
@@ -148,7 +148,7 @@ void DroneNavigation::update(double time) {
             _visdat->enable_collect_no_drone_frames = true;
             if (time-time_located_drone>2 && (_dctrl->drone_state_disarmed() || pparams.joystick != rc_none)) { // delay until blinking stopped. Drone must be 1.5s disarmed to get out of a possible (rx) failsafe
                 _visdat->enable_background_motion_map_calibration(motion_calibration_duration);
-                _visdat->create_overexposed_removal_mask(_trackers->dronetracker()->drone_takeoff_im_location(),_trackers->dronetracker()->drone_takeoff_im_size());
+                _visdat->create_overexposed_removal_mask(_trackers->dronetracker()->takeoff_im_location(),_trackers->dronetracker()->takeoff_im_size());
                 time_motion_calibration_started = time;
                 _navigation_status = ns_calibrating_motion;
                 _dctrl->start_landing_acc_calibration();
@@ -208,6 +208,7 @@ void DroneNavigation::update(double time) {
                     break;
                 } else if (_trackers->too_many_false_positives()) {
                     _navigation_status = ns_tracker_problem;
+                    break;
                 }
 
                 _trackers->mode(tracking::TrackerManager::mode_wait_for_insect);
@@ -454,7 +455,7 @@ void DroneNavigation::update(double time) {
         } case ns_landing: {
             cv::Point3f new_pos_setpoint;
             cv::Point3f new_vel_setpoint;
-            cv::Point3f pad_pos = _trackers->dronetracker()->drone_landing_location(true);
+            cv::Point3f pad_pos = _trackers->dronetracker()->landing_location(true);
             setpoint_pos_world =  pad_pos + current_waypoint->xyz;
             if (setpoint_pos_world.y > -0.5f) // keep some margin to the top of the view, because atm we have an overshoot problem.
                 setpoint_pos_world.y = -0.5f;
@@ -550,11 +551,11 @@ void DroneNavigation::next_waypoint(Waypoint wp, double time) {
     delete current_waypoint;
     current_waypoint = new Waypoint(wp);
     if (wp.mode == wfm_takeoff) {
-        cv::Point3f p = _trackers->dronetracker()->drone_takeoff_location();
+        cv::Point3f p = _trackers->dronetracker()->takeoff_location();
         setpoint_pos_world =  p + wp.xyz;
         setpoint_pos_world = _camview->setpoint_in_cameraview(setpoint_pos_world, CameraView::relaxed);
     } else if (wp.mode == wfm_landing || wp.mode == wfm_yaw_reset) {
-        cv::Point3f p = _trackers->dronetracker()->drone_landing_location(true);
+        cv::Point3f p = _trackers->dronetracker()->landing_location(true);
         setpoint_pos_world =  p + wp.xyz;
         if (setpoint_pos_world.y > -0.5f) // keep some margin to the top of the view, because atm we have an overshoot problem.
             setpoint_pos_world.y = -0.5f;

src/navigation/dronenavigation.h
@@ -184,9 +184,9 @@ public:
 
         cv::Point3f tmp = setpoint_pos_world;
         if (!drone_is_flying()) {
-            tmp.x = _trackers->dronetracker()->drone_takeoff_location().x;
-            tmp.y = _trackers->dronetracker()->drone_takeoff_location().y+0.5f;
-            tmp.z = _trackers->dronetracker()->drone_takeoff_location().z;
+            tmp.x = _trackers->dronetracker()->takeoff_location().x;
+            tmp.y = _trackers->dronetracker()->takeoff_location().y+0.5f;
+            tmp.z = _trackers->dronetracker()->takeoff_location().z;
         }
 
         cv::Point3f resf  =world2im_3d(tmp,_visdat->Qfi,_visdat->camera_angle);

src/navigation/interceptor.cpp
@@ -136,7 +136,7 @@ cv::Point3f Interceptor::update_far_target(bool drone_at_base) {
     cv::Point3f drone_pos = dtd.pos();
 
     if (drone_at_base)
-        drone_pos = _trackers->dronetracker()->drone_takeoff_location();
+        drone_pos = _trackers->dronetracker()->takeoff_location();
 
     cv::Point3f drone_vel = dtd.vel();
     calc_tti(predicted_pos, _aim_vel, drone_pos, drone_vel, drone_at_base); // only used for viz _tti

commit 0cb2e9e298f267e38877d31046c217a27193d366

[CV] Enhancement: before takeoff draw the (predicted) drone location as takeoff location. Regression from 6b1c52461aaee2c101085bdafec586029ea55624

src/vizs.cpp
@@ -474,6 +474,8 @@ void Visualizer::draw_tracker_viz() {
     if ( last_drone_detection.predicted_image_item.valid) {
         auto pred =  last_drone_detection.predicted_image_item;
         cv::circle(frameL_color,pred.pt,pred.size/2,cv::Scalar(0,255,0));
+    } else if(_trackers->dronetracker()->takeoff_location_valid()) {
+        cv::circle(frameL_color,_trackers->dronetracker()->takeoff_im_location(),_trackers->dronetracker()->takeoff_im_size()/2,cv::Scalar(0,255,0));
     }
     if ( last_drone_detection.world_item.iti.valid) {
         auto p =  last_drone_detection.world_item.iti;
@@ -563,6 +565,7 @@ void Visualizer::draw_tracker_viz() {
             cv::line(frameL_color,drone_pos,tv,cv::Scalar(0,255,0),1);
         }
     }
+
     cv::resize(frameL_color,roi,size);
 
     cv::Size vizsizeL(size.width/4,size.height/4);