c64e3b4530b6c13699e0d80a21b6646657e6a0f2
Jorn96 <jorn@pats-drones.com>
2021-02-17 11:40:57 +0100

    Closes #712 added in progress and under maintenance to daily digest

--- dashboard/daily_digest.py
+++ dashboard/daily_digest.py
@@ -2,7 +2,8 @@
 import socket, json,shutil,sqlite3,subprocess
 import time, argparse
 import pickle, glob, os, re
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
+import numpy as np
 
 def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
@@ -18,12 +19,19 @@ def natural_sort_systems(l):
 def load_systems():
     conn = sqlite3.connect(os.path.expanduser(args.db_path))
     cur = conn.cursor()
-    sql_str = '''SELECT system_name FROM systems WHERE is_active = 1'''
+    sql_str = '''SELECT system_name,first_warning,maintenance FROM systems WHERE is_active = 1 ORDER BY system_id'''
     cur.execute(sql_str)
-    systems = cur.fetchall()
-    systems = natural_sort_systems(systems)
-    systems = [d[0] for d in systems]
+    systems = np.array(cur.fetchall(),dtype=object)
     return systems
+
+def write_systems(changed_systems):
+    conn = sqlite3.connect(os.path.expanduser(args.db_path))
+    cur = conn.cursor()
+    sql_str = '''UPDATE systems SET first_warning=?, maintenance=? WHERE system_name=? '''
+    for sys,date,maintenance in changed_systems:
+        cur.execute(sql_str,(date,maintenance,sys))
+    conn.commit()
+    conn.close()
 
 def execute(cmd):
     p_result = None
@@ -42,9 +50,11 @@ def execute(cmd):
 def send_mail(now):
     files = natural_sort([fp for fp in glob.glob(os.path.expanduser(args.input_folder + "/*.processed"))])
     systems = load_systems()
-
     mail_txt = ''
     recent_files = []
+    changed_systems = []
+    in_progress = []
+    under_maintenance = []
     for file in files:
         f = os. path. splitext(os.path.basename((file)))[0]
         if f.startswith('pats'):
@@ -56,10 +66,13 @@ def send_mail(now):
                 if d > now - timedelta(hours=5):
 
                     with open (file, "r") as fr_processed:
-                        if f_sys in systems:
+                        if f_sys in systems[:,0]:
                             msg = fr_processed.readline()
                             recent_files.append([f_sys,msg])
-                            systems.remove(f_sys)
+                            full_system = systems[systems[:,0]==f_sys][0]
+                            if full_system[1] is not None or full_system[2]:
+                                changed_systems.append((full_system[0],None,0))
+                            systems = np.delete(systems, np.argwhere(systems[:,0]==f_sys), axis= 0)
                         else:
                             print('Warning, system does not exist: ' + f_sys)
             except:
@@ -67,8 +80,19 @@ def send_mail(now):
     mail_txt = 'Pats status report ' + str(now) + '\n\n'
     if (len(systems)>0):
         mail_txt += 'WARNINGS: \nNothing received from: '
-        for sys in systems:
-            mail_txt += sys + ', '
+        for (sys,date,maintenance) in systems:
+            if not date:
+                date = datetime.today().strftime('%Y%m%d')
+                mail_txt += sys + ', '
+                changed_systems.append((sys,date,maintenance))
+            elif maintenance:
+                date = datetime.strptime(date,'%Y%m%d')
+                if datetime.today() - date <= timedelta(days=3):
+                    in_progress.append(sys)
+                else:
+                    under_maintenance.append(sys)
+            else:
+                mail_txt += sys + ', '
         mail_txt = mail_txt[:-2] + '\n'
         for sys in recent_files:
             if not sys[1].startswith('OK.'):
@@ -79,6 +103,13 @@ def send_mail(now):
     for sys in recent_files:
         if sys[1].startswith('OK. '):
             mail_txt += sys[0] + ':\t' + sys[1].replace('OK.','') + '\n'
+    mail_txt += '\nSystems in progress:\n'
+    for sys in in_progress:
+        mail_txt += sys + ', '
+    mail_txt = mail_txt[:-2] + '\n\nSystems under maintenance:\n'
+    for sys in under_maintenance:
+        mail_txt += sys + ', '
+    mail_txt = mail_txt[:-2]
 
     print(mail_txt)
     with open('mail.tmp','w') as mail_f:
@@ -87,6 +118,7 @@ def send_mail(now):
     cmd = 'mail -s "Pats daily status digest" sys-maintenance@pats-drones.com < mail.tmp'
     execute(cmd)
 
+    write_systems(changed_systems)
 
 parser = argparse.ArgumentParser(description='Script that adds the json files or incoming json files to the database that is reable for the electron app.')
 parser.add_argument('-i', '--input_folder', help="Path to the folder with json files", default='~/jsons/')
