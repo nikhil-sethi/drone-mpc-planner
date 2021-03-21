fe85dee3a9e4078b84681d30ed013ce1082f7de5
Kevin <kevindehecker@hotmail.com>
2021-03-21 20:29:50 +0100

    Give nethogs screen proper name

--- base/install/screens.sh
+++ base/install/screens.sh
@@ -13,7 +13,7 @@ done
 mkdir -p ~/pats/logs
 
 # Start other screens
-/usr/bin/screen -t upl /bin/bash -c 'sudo nethogs -v3; exec /bin/bash'
+/usr/bin/screen -t hogs /bin/bash -c 'sudo nethogs -v3; exec /bin/bash'
 /usr/bin/screen -t upl /bin/bash -c 'cd ~/code/pats/base/data_processing && ~/code/pats/base/data_processing/daemon.py; exec /bin/bash'
 /usr/bin/screen -t pats /bin/bash -c 'cd ~/code/pats/base/data_processing && ~/code/pats/base/install/autostart.sh 2>&1 | /usr/bin/tee -a ~/pats/logs/term.log; exec /bin/bash'
 /usr/bin/screen -t bash /bin/bash -c 'cd ~/; exec /bin/bash'
