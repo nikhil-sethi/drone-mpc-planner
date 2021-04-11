97b7993013aa973bab4ea4290a025661369ef754
Kevin <kevindehecker@hotmail.com>
2021-04-11 22:40:46 +0200

    [Control] TMP HACK disabled prop wash feature

--- base/src/propwashhandler.cpp
+++ base/src/propwashhandler.cpp
@@ -17,13 +17,14 @@ bool PropwashHandler::prop_wash(cv::Point3f drone_velocity, cv::Point3f des_acc_
         return false;
 }
 
-cv::Point3f PropwashHandler::update(cv::Point3f drone_velocity, cv::Point3f desired_acc, float thrust){
+cv::Point3f PropwashHandler::update(cv::Point3f drone_velocity, cv::Point3f desired_acc, float thrust) {
     _propwash = prop_wash(drone_velocity, desired_acc);
 
     float control_margin = GRAVITY * tanf(acosf(GRAVITY/thrust)); // see doc/control-margin.svg
     if(_propwash) {
-        desired_acc /= normf(desired_acc);
-        desired_acc *= 0.7f*control_margin;
+        std::cout << "Sodan" << std::endl;
+        // desired_acc /= normf(desired_acc);
+        // desired_acc *= 0.7f*control_margin;
     }
 
     return desired_acc;
