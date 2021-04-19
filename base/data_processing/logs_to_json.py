9d92bc65bbb96da124d3b54e43c7bebb6670dcc6
pats-proto9 <pats-proto9@pats.com>
2021-04-19 13:39:55 +0200

    [Bug] Systems do not have matplotlib libary, commented import because is only used for feature we use.

--- base/data_processing/logs_to_json.py
+++ base/data_processing/logs_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os,glob,json,math,argparse,socket,logging,subprocess,shutil
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pathlib import Path
