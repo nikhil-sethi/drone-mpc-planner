067550574324b4f793684958f0ebdb4ea281ed12
pats-proto0 <pats-proto0@pats.com>
2021-05-03 21:36:21 +0200

    [renders] Add PCH to wifi render systems

--- dashboard/watch_config/downloader.py
+++ dashboard/watch_config/downloader.py
@@ -38,6 +38,12 @@ def download_renders_all():
     download_renders(38)
     download_renders(39)
 
+    download_renders(52)
+    download_renders(66)
+    download_renders(68)
+    download_renders(69)
+
+
 def natural_sort(l):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
