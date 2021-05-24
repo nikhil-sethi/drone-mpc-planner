cf8c22897f498d0b6494bc3f874ab3e0c1950bfe
Kevin <kevindehecker@hotmail.com>
2021-05-24 11:15:50 +0200

    [Admin] Add timestamps in tunnel logs

--- base/install/install.sh
+++ base/install/install.sh
@@ -53,7 +53,7 @@ pushd ~/dependencies
 # Install pats dependency packages
 [ -f dependencies-packages-v1.13.done ] || {
 	sudo apt update
-	sudo apt install -y build-essential g++ gdb libva-dev libswresample-dev libavutil-dev pkg-config libcurl4-openssl-dev ncdu openssh-server ffmpeg unattended-upgrades inotify-tools cpputest python3-pip dfu-util exfat-utils vnstat ifmetric net-tools lm-sensors nethogs htop git nano screen autossh usb-modeswitch
+	sudo apt install -y build-essential g++ gdb libva-dev libswresample-dev libavutil-dev pkg-config libcurl4-openssl-dev ncdu openssh-server ffmpeg unattended-upgrades inotify-tools cpputest python3-pip dfu-util exfat-utils vnstat ifmetric net-tools lm-sensors nethogs htop git nano screen autossh usb-modeswitch moreutils
 	sudo apt install -y gstreamer1.0-tools gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-plugins-bad gstreamer1.0-libav libgstreamer-plugins-base1.0-0 libgstreamer-plugins-bad1.0-0 libgstreamer-plugins-good1.0-0 gstreamer1.0-vaapi vainfo
 	sudo apt-get remove -y modemmanager
 	pip3 install cython pyserial
--- base/install/tunnel.sh
+++ base/install/tunnel.sh
@@ -19,9 +19,11 @@ SSH_PORT=${2:-22}
 echo "Creating tunnel at $IP:$SSH_PORT with port $PORT"
 while [ 1 ]; do
  	
- 	echo waiting 10s for starting tunnel
+ 	dt=$(date '+%d/%m/%Y %H:%M:%S');
+	echo "${dt}: waiting 10s for starting tunnel"
  	sleep 10s # sleep first so that if the wifi is still being conencted, that happens before the tunnel is created
+	
 
- 	/usr/bin/autossh -M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 180" -o "ServerAliveCountMax 3" -C4NR $PORT:localhost:22 $USER@$IP -p $SSH_PORT
+ 	/usr/bin/autossh -M 0 -o "ExitOnForwardFailure yes" -o "ServerAliveInterval 180" -o "ServerAliveCountMax 3" -C4NR $PORT:localhost:22 $USER@$IP -p $SSH_PORT 2>&1 | ts
 
 done
