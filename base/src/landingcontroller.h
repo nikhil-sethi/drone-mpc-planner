91048ab9d780c04e5527c4272c1d6f7e61e0e609
Ludwig <ludwig@pats-drones.com>
2021-10-12 19:54:39 +0200

    Reduce feedforward land time

--- base/src/landingcontroller.h
+++ base/src/landingcontroller.h
@@ -8,7 +8,7 @@ private:
     // Parameter:
     const float target_descend_vel = -0.35f;
     const float height_trigger_ff = 0.1f;
-    const float _time_ff_landing = 0.3f;
+    const float _time_ff_landing = 0.1f;
 
 public:
     cv::Point3f setpoint_cc_landing(cv::Point3f pad_location, navigation::Waypoint *current_waypoint, float dt_land);
