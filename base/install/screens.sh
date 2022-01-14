2977082795da6a1a36d8b7bec0d3a1bbf47ba5ee
LudwigKr <ludwigkr@mailbox.org>
2022-01-14 18:19:25 +0100

    Autonmatically call moth trigger script

--- base/install/screens.sh
+++ base/install/screens.sh
@@ -17,4 +17,5 @@ mkdir -p ~/pats/logs
 /usr/bin/screen -t upl /bin/bash -c 'cd ~/code/pats/base/data_processing && ~/code/pats/base/data_processing/auto_daemon_starter.py; exec /bin/bash'
 /usr/bin/screen -t bb /bin/bash -c 'cd ~/code/pats/base/data_processing && ~/code/pats/base/data_processing/auto_baseboard_daemon_starter.py; exec /bin/bash'
 /usr/bin/screen -t exe /bin/bash -c 'cd ~/code/pats/base/install && ~/code/pats/base/install/autostart.sh 2>&1 | /usr/bin/tee -a ~/pats/logs/term.log; exec /bin/bash'
+#/usr/bin/screen -t trig-m /bin/bash -c 'cd ~/code/pats/config && ~/code/pats/config/periodic_virtual_insects.sh; exec /bin/bash'
 /usr/bin/screen -t bash /bin/bash -c 'cd ~/; exec /bin/bash'
