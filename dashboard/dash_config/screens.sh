d3299fd7ec34e85ba5536ca5195a9cd92f4a81f8
Kevin <kevindehecker@hotmail.com>
2022-05-21 09:22:23 +0200

    [admin] Move dashboard to seperate repo

--- dashboard/dash_config/screens.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-/usr/bin/screen -t down /bin/bash -c 'cd ~/code/pats/dashboard && ./dash_daemon.py; exec /bin/bash'
-/usr/bin/screen -t mail /bin/bash -c 'cd ~/code/pats/dashboard && ./daily_digest.py; exec /bin/bash'
-/usr/bin/screen -t patsc /bin/bash -c 'cd ~/code/pats/dashboard &&  docker logs --follow dashboard_patsc_1 ; exec /bin/bash'
-/usr/bin/screen -t bash /bin/bash -c 'cd ~ ; exec /bin/bash'
