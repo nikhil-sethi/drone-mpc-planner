5b30ade13eee5552af52e434dc1c6b256eddde96
Kevin <kevindehecker@hotmail.com>
2022-08-01 23:23:09 +0200

    [ansible] Add more checks to checks. Improve speed 30x

--- base/ansible/playbooks/checks.sh
+++ base/ansible/playbooks/checks.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-uptime -p
-cd ~/code/pats && echo git sha: $(git rev-parse HEAD)
-echo image version: $(cat ~/dependencies/image_version) kernel: $(uname -r) $(lsb_release -d)
-lscpu | grep 'Model name' &&
-if [ -f /var/run/reboot-required ]; then echo 'reboot required'; fi
-if [ -f '/home/pats/pats/xml/pats.xml' ] ; then echo 'Custom pats.xml found' ; fi
-if ( ! grep -q "Version=\"1.20\"" /home/pats/pats/xml/pats.xml ) ; then echo pats.xml VERSION ERROR ; fi
-if [ -e '/dev/baseboard' ] && [ -f '/home/pats/pats/flags/disable_baseboard' ] ; then echo 'Baseboard AVAILABLE BUT DISABLED' ; fi
-if [ ! -e '/dev/baseboard' ] && [ ! -f '/home/pats/pats/flags/disable_baseboard' ] ; then echo 'Baseboard NOT DETETECTED AND NOT DISABLED' ; fi
-if [ -e '/dev/pats_mm0' ] ; then echo 'MultiModule: OK' ; else echo 'MultiModule: NOT DETECTED' ; fi
-if ( rs-enumerate-devices | grep -q plugged ) ; then echo NO REALSENSE ; fi
+# uptime -p
+# cd ~/code/pats && echo git sha: $(git rev-parse HEAD)
+# echo image version: $(cat ~/dependencies/image_version) kernel: $(uname -r) $(lsb_release -d)
+# lscpu | grep 'Model name' &&
+if [ -f /var/run/reboot-required ]; then echo 'Reboot required'; fi
+if [ -f '/home/pats/pats/xml/pats.xml' ] ; then
+    if ( ! grep -q "Version=\"1.20\"" /home/pats/pats/xml/pats.xml ) ; then echo pats.xml VERSION ERROR ; fi
+fi
+if [ -e '/dev/baseboard' ] && [ -f '/home/pats/pats/flags/disable_baseboard' ] ; then echo 'BASEBOARD AVAILABLE BUT DISABLED' ; fi
+if [ ! -e '/dev/baseboard' ] && [ ! -f '/home/pats/pats/flags/disable_baseboard' ] ; then echo 'BASEBOARD NOT DETETECTED AND NOT DISABLED' ; fi
+#if [ -e '/dev/pats_mm0' ] ; then echo 'MultiModule: OK' ; else echo 'MULTIMODULE NOT DETECTED' ; fi
+if ( rs-enumerate-devices | grep -q plugged ) ; then echo REALSENSE NOT DETECTED ; fi
+if ( ! ps -aux  | grep -q "[b]aseboardlink.py" ) ; then echo BASEBOARDLINK NOT RUNNING ; fi
+if ( ! ps -aux  | grep -q "[d]aemon.py" ) ; then echo DAEMON NOT RUNNING ; fi
 if [ ! -f /home/pats/code/pats/base/build/executor ]; then echo 'EXECUTOR DOES NOT EXIST'; fi
-if ( ! ps -aux  | grep -q "[e]xecutor" ) ; then echo NO EXECUTOR ; fi
-if ( cd /home/pats/code/pats && git rev-parse HEAD | grep -q 5925856ef1e87bac1e77700d9bc52fa0457644c2 ) ; then echo sha ok ; else echo SHA DISCREPANCY; fi
+if ( ! ps -aux  | grep -q "[e]xecutor" ) ; then echo EXECUTOR NOT RUNNING ; fi
+if ! ( cd /home/pats/code/pats && git rev-parse HEAD | grep -q 02ef06a89c29b3f7f9f86af346221c1bdd1e9c47 ) ; then echo SHA DISCREPANCY; fi
