6b50e1f96b0ab0601e26fdd9dd13720f68c9ac6f
Kevin <kevindehecker@hotmail.com>
2022-08-08 22:24:19 +0200

    [admin] Use the correct ssh config for product serial db reading

--- base/install/set_hostname.py
+++ base/install/set_hostname.py
@@ -10,9 +10,9 @@ from pathlib import Path
 if not os.path.isfile('/home/pats/dependencies/hostname_set'):
 
     time.sleep(5) # seems to be necessary with old ubuntu 18 images, need some time to get wifi
-    
+
     id_db_path = '/home/pats/pats/id_db.txt'
-    rsync_cmd = ['rsync -a -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR  -i /home/pats/.ssh/id_rsa -F /home/pats/.ssh/config" ' + 'dash:id_db.txt ' + id_db_path]
+    rsync_cmd = ['rsync -a -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR  -i /home/pats/.ssh/id_rsa -F /home/pats/code/pats/base/install/sshconfig" ' + 'dash:id_db.txt ' + id_db_path]
     subprocess.run(rsync_cmd, shell=True)
 
     with open(id_db_path, 'r', encoding="utf-8") as sysdb:
@@ -22,7 +22,7 @@ if not os.path.isfile('/home/pats/dependencies/hostname_set'):
             if line.split(' ')[0] == ser:
                 print('FOUND:' + line)
                 pats_id = line.split(' ')[1]
-                
+
                 subprocess.run('hostnamectl set-hostname ' + pats_id, shell=True)
 
                 with open('/home/pats/dependencies/hostname_set', 'w', encoding="utf-8") as touchy_file:
@@ -30,4 +30,4 @@ if not os.path.isfile('/home/pats/dependencies/hostname_set'):
                 exit(0)
 
     print('Error: product id not found')
-    
+
