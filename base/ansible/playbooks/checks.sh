bb1e2b3b5d29f7b1897fd5aaf801d5d1fec5cc29
Kevin <kevindehecker@hotmail.com>
2022-08-10 16:16:05 +0200

    [ansible] trivial

--- base/ansible/playbooks/checks.sh
+++ base/ansible/playbooks/checks.sh
@@ -4,7 +4,8 @@
 # cd ~/code/pats && echo git sha: $(git rev-parse HEAD)
 # echo image version: $(cat ~/dependencies/image_version) kernel: $(uname -r) $(lsb_release -d)
 # lscpu | grep 'Model name' &&
-# tail ~/pats/logs/all_errors.log.20220803 -n +2
+tail ~/pats/logs/all_errors.log.20220809 -n +2
+if [ -f '/home/pats/pats/flags/disable' ] ; then echo 'SYSTEM DISABLED' ; fi
 if [ -f /var/run/reboot-required ]; then echo 'Reboot required'; fi
 if [ -f '/home/pats/pats/xml/pats.xml' ] ; then
     if ( ! grep -q "Version=\"1.20\"" /home/pats/pats/xml/pats.xml ) ; then echo pats.xml VERSION ERROR ; fi
@@ -17,4 +18,4 @@ if ( ! ps -aux  | grep -q "[b]aseboardlink.py" ) ; then echo BASEBOARDLINK NOT R
 if ( ! ps -aux  | grep -q "[d]aemon.py" ) ; then echo DAEMON NOT RUNNING ; fi
 if [ ! -f /home/pats/code/pats/base/build/executor ]; then echo 'EXECUTOR DOES NOT EXIST'; fi
 if ( ! ps -aux  | grep -q "[e]xecutor" ) ; then echo EXECUTOR NOT RUNNING ; fi
-if ! ( cd /home/pats/code/pats && git rev-parse HEAD | grep -q 9b61783b12fbd1677e2910e8ddfb4c6d8227239f ) ; then echo SHA DISCREPANCY; fi
+if ! ( cd /home/pats/code/pats && git rev-parse HEAD | grep -q 1c2f332de90b0f1c49e90ac5d012c0703aff91e0 ) ; then echo SHA DISCREPANCY; fi
