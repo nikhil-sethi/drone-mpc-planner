d54efda0d8b7e450a2a134df6a693bebd213670e
Jorn96 <jorn@pats-drones.com>
2022-08-10 11:36:11 +0200

    Systems load their timezone from dash now

--- base/install/daemon.sh
+++ base/install/daemon.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/env bash
 
 python3 /home/pats/code/pats/base/install/set_hostname.py || true
+python3 /home/pats/code/pats/base/install/set_timezone.py || true
+
 
 set -e
 # add /home/pats/code/pats/base/install/daemon.sh to /etc/rc.local for autostart
 # this script creates a screen session that runs as non-elevated user pats
-# a seperate screen window is added for each process 
+# a seperate screen window is added for each process
 # use screen -r to re-attach to below screen session later, ctrl a,  <number> to go to the <number> window
 user=pats
 
