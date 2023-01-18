fd7657b59ca6c2e9d8fdebd9300bf5678dc26bcf
Rik <rikjbouwmeester@gmail.com>
2023-01-18 16:40:16 +0100

    Guarantee ROI to be larger than template.
    Overloads rectangle clamping with a function that can shift the resulting rectangle to ascertain a minimum rectangle height and width. Fix for issue #1337

--- base/src/common.cpp
+++ base/src/common.cpp
@@ -243,6 +243,28 @@ cv::Rect clamp_rect(cv::Rect r, int w, int h) {
     return r;
 }
 
+cv::Rect clamp_rect(cv::Rect r, int w_min, int h_min, int w_max, int h_max) {
+    r.x = std::clamp(r.x, 0, w_max - 1); // rect width must be atleast 1, so w-1
+    r.y = std::clamp(r.y, 0, h_max - 1); // idem
+    if (r.x + r.width >= w_max)
+        r.width = w_max - r.x;
+    if (r.y + r.height >= h_max)
+        r.height = h_max - r.y;
+
+    // shift and increase size of rect if it is too small
+    if (r.width < w_min) {
+        int delta = w_min - (r.width);
+        r.x = std::clamp(r.x - delta, 0, w_max - 1);
+        r.width = w_min;
+    }
+    if (r.height < h_min) {
+        int delta = h_min - (r.height);
+        r.y = std::clamp(r.y - delta, 0, h_max - 1);
+        r.height = h_min;
+    }
+    return r;
+}
+
 std::string to_string_with_precision(float f, const int n) {
     std::ostringstream out;
     out << std::fixed << std::setprecision(n) << f;
