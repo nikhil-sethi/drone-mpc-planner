77e78c3643819717c269a13a025490f2d5d015ad
Rik <rikjbouwmeester@gmail.com>
2023-01-23 11:48:52 +0100

    Resume benchmark after restart

--- base/src/drone.h
+++ base/src/drone.h
@@ -214,6 +214,7 @@ public:
     }
     bool low_voltage_timeout(double time);
 
+    bool benchmark_mode = false;
     int benchmark_len = 0;
     int benchmark_entry_id = 0;
     time_t benchmark_time;
