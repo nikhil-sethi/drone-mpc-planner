68411b699336a72b8f301d9549ce1dfeb7cc3348
Kevin <kevindehecker@hotmail.com>
2023-03-10 11:18:37 +0100

    [admin] wireguard config, skip 0 ips apparantely

--- config/generate_ssh_conf.py
+++ config/generate_ssh_conf.py
@@ -9,8 +9,8 @@ import time
 with open("pats_ssh_config", "w") as f:
     for i in range(0, 999):
         f.write('host pats' + str(i))
-        i1 = int(10 + (i - (i % 256)) / 256)
-        i2 = i % 256
+        i1 = int(10 + (i - (i % 255)) / 255)
+        i2 = i % 255
         f.write('\n\tHostname 10.13.' + str(i1) + '.' + str(i2))
         f.write('\n\tIdentityFile ~/.ssh/pats_wg_id_ed25519')
         f.write('\n\tUser pats\n\n')
