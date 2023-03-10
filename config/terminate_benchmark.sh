53217209e5f5d25ba993cd452e14f5ec1506581f
Rik <rikjbouwmeester@gmail.com>
2023-03-10 15:55:16 +0100

    Add script to terminate benchmark

---
+++ config/terminate_benchmark.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ENTRY_FILE=/home/pats/pats/flags/BenchmarkEntry.txt
+BENCHMARK_OVERVIEW_FILE=/home/pats/pats/flags/pats_benchmark_trigger.csv
+BENCHMARK_TRIGGER=/home/pats/pats/flags/benchmark.csv
+
+rm $ENTRY_FILE
+rm $BENCHMARK_OVERVIEW_FILE
+rm $BENCHMARK_TRIGGER
