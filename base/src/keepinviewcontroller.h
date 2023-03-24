b3f39c0a13319516ae331e0edcee878efab0cdca
Rik <rikjbouwmeester@gmail.com>
2023-03-24 12:39:33 +0100

    [experimental] strongly reduce KIV safety factor
    As KIV kicks in very early and affects hunting performance

--- base/src/keepinviewcontroller.h
+++ base/src/keepinviewcontroller.h
@@ -58,7 +58,7 @@ private:
 public:
     // Parameter:
     float drone_rotating_time;
-    const float safety = 2.f;
+    const float safety = 1.3f;
 
     // States:
     bool active = false;  //indicator for the log, whether kiv is active or not
