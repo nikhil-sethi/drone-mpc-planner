f1a75cf289c290602c1961c13b013777eb2be75f
Rik <rikjbouwmeester@gmail.com>
2023-03-28 10:40:27 +0200

    Expose hash

--- base/src/benchmark/benchmarkreader.cpp
+++ base/src/benchmark/benchmarkreader.cpp
@@ -1,23 +1,11 @@
 #include "benchmarkreader.h"
+#include "hash.h"
 #include<fstream>
 #include <iostream>
 #include <sstream>
 
 
-struct HashableObj {
-    std::string string;
-    std::size_t previous_hash;
-};
 
-struct MyHash
-{
-    std::size_t operator()(HashableObj const &s) const noexcept
-    {
-        std::size_t h1 = std::hash<std::string> {}(s.string);
-        std::size_t h2 = s.previous_hash;
-        return h1 ^ (h2 << 1);
-    }
-};
 // BenchmarkReader reader;
 std::vector<BenchmarkEntry> benchmark_entries;
 
