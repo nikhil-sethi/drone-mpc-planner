22296a5b2d79f5f465481fe82a4cb9b377821f41
Kevin <kevindehecker@hotmail.com>
2023-03-29 22:40:02 +0200

    [admin] Upgrade release system to dev system script

---
+++ config/install_dev.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+# Install the Pats code
+# You need to manually rsync a ssh key to the system at ~/.ssh/id_rsa.pats  first!
+# e.g. rsync ~/.ssh/id_rsa.pats pats34:.ssh/ -a
+
+
+mkdir -p ~/code/
+cd ~/code
+git clone git@github-pats:pats-drones/pats.git
+cd pats
+mkdir -p base/build
+cd base/build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j$(nproc)
+
+
+
+rm ~/pats/release/build -rf
+rm ~/pats/release/install -rf
+rm ~/pats/release/scripts -rf
+rm ~/pats/release/xml -rf
+ln -s ~/code/pats/base/build ~/pats/release/build
+ln -s ~/code/pats/base/install ~/pats/release/install
+ln -s ~/code/pats/base/scripts ~/pats/release/scripts
+ln -s ~/code/pats/base/xml ~/pats/release/xml
+
+
+
