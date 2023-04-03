893ebc052fe92f7cbd35bada0635c050c3a1b04d
Rik <rikjbouwmeester@gmail.com>
2023-04-03 14:28:01 +0200

    Strictly enforce aim within view when interception or stopping position are outside of flight area.

--- base/src/navigation/interceptor.cpp
+++ base/src/navigation/interceptor.cpp
@@ -256,6 +256,7 @@ void Interceptor::update_hunt_strategy(bool drone_at_base, tracking::TrackData t
                 if (!interception_position_in_flightarea || !stopping_position_in_flightarea) {
                     // Insect is currently not interceptable. Try to go directly to the target (and hope is target changing its path)
                     _aim_pos = target.pos() + _tti * target.vel(); //+ 0.4 * (target.pos() - drone.pos()) * hunt_error;
+                    _aim_pos = _flight_area->move_inside(_aim_pos, strict, drone.pos());
                     _control_mode = position_control;
                     return;
                 } else {
