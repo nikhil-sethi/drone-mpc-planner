b4de2034c8a661aa2c65983346a435fa9a5600ef
Kevin <kevindehecker@hotmail.com>
2023-05-12 10:53:34 +0200

    [ansible] Fix some version string discrepancies

--- base/ansible/playbooks/checks.sh
+++ base/ansible/playbooks/checks.sh
@@ -7,7 +7,7 @@
 if [ -f '/home/pats/pats/flags/disable' ] ; then echo 'SYSTEM DISABLED' ; fi
 if [ -f /var/run/reboot-required ]; then echo 'Reboot required'; fi
 if [ -f '/home/pats/pats/xml/pats.xml' ] ; then
-    if ( ! grep -q "Version=\"1.20\"" /home/pats/pats/xml/pats.xml ) ; then echo pats.xml VERSION ERROR ; fi
+    if ( ! grep -q "Version=\"1.21\"" /home/pats/pats/xml/pats.xml ) ; then echo pats.xml VERSION ERROR ; fi
 fi
 if [ -e '/dev/baseboard' ] && [ -f '/home/pats/pats/flags/disable_baseboard' ] ; then echo 'BASEBOARD AVAILABLE BUT DISABLED' ; fi
 if [ ! -e '/dev/baseboard' ] && [ ! -f '/home/pats/pats/flags/disable_baseboard' ] ; then echo 'BASEBOARD NOT DETETECTED AND NOT DISABLED' ; fi
@@ -18,6 +18,6 @@ if [ ! -f '/home/pats/pats/flags/disable_executor' ] && ( ! ps -aux  | grep -q "
 if ( ! ps -aux  | grep -q "[b]aseboardlink.py" ) ; then echo BASEBOARDLINK NOT RUNNING ; fi
 if ( ! ps -aux  | grep -q "[d]aemon.py" ) ; then echo DAEMON NOT RUNNING ; fi
 if ( ! ifconfig | grep -q "inet 10.13" ) ; then echo WIREGUARD NOT CONNECTED; fi
-if [ -f '/home/pats/dependencies/release_upgraded_20230405.done' ] ; then if ! ( cd /home/pats/pats/release && git describe --tags | grep -q "v2.2.2" ) ; then echo TAG DISCREPANCY; fi ; fi
+if [ -f '/home/pats/dependencies/release_upgraded_20230405.done' ] ; then if ! ( cd /home/pats/pats/release && git describe --tags | grep -q "v2.3.3" ) ; then echo TAG DISCREPANCY; fi ; fi
 if [ ! -f '/home/pats/dependencies/release_upgraded_20230405.done' ] && [ -f '/home/pats/dependencies/release_upgraded_P1_20230405.done' ] ; then echo 'RELEASE UPGRADE NOT FINISHED' ; fi
 if [ ! -f '/home/pats/dependencies/release_upgraded_20230405.done' ] && [ ! -f '/home/pats/dependencies/release_upgraded_P1_20230405.done' ] ; then echo 'RELEASE UPGRADE NOT DONE' ; fi
