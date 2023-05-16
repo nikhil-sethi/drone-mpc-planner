01e54123a01fd5e949945c45d5ed403218d936a1
Jorn96 <jorn@pats-drones.com>
2023-05-16 14:18:22 +0200

    [bug] hunt id was int64 instead of int

--- base/scripts/process_session.py
+++ base/scripts/process_session.py
@@ -388,7 +388,7 @@ def process_detection_log(log_fn: str, folder: str, session_start_datetime: date
         hunt_id_df = log['hunt_id'].dropna()
         hunt_id_df = hunt_id_df.drop(hunt_id_df[hunt_id_df <= 0].index)
         if not hunt_id_df.empty:
-            hunt_id = hunt_id_df.groupby(hunt_id_df).size().agg(['idxmax', 'max'])[0]
+            hunt_id = int(hunt_id_df.groupby(hunt_id_df).size().agg(['idxmax', 'max'])[0])
             drone_flight = list(filter(lambda item: item['flight_id'] == hunt_id, flights_in_folder))
             if not drone_flight:
                 hunt_id = -1
