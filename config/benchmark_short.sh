91d4e7a08121d97f990a7829474ee3adffb748b3
Rik <rikjbouwmeester@gmail.com>
2023-05-25 12:21:57 +0200

    Rename benchmark_short to benchmark
    How can it be short if is also the longest?

--- config/benchmark_short.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-printf 'Starting benchmark in 3 seconds...\n'
-sleep 3
-cp ~/code/pats/base/benchmark/benchmark_short.csv ~/pats/flags/benchmark.csv
--- config/infinite_benchmarks.sh
+++ config/infinite_benchmarks.sh
@@ -7,7 +7,7 @@ while true; do
     if test -f "$FILE"; then
         echo "Benchmark running."
     else
-        bash benchmark_short.sh
+        bash benchmark.sh
     fi
     sleep $TRIGGER_DELAY
 done
