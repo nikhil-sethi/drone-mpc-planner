2348cbdf7aa23f49da44c377251a8bc80410ceda
Rik <rikjbouwmeester@gmail.com>
2023-06-14 15:23:04 +0200

    Go back to the silly position control hack.

--- base/src/navigation/interceptor.cpp
+++ base/src/navigation/interceptor.cpp
@@ -226,6 +226,7 @@ void Interceptor::update_hunt_strategy(bool drone_at_base, tracking::TrackData t
                     _aim_pos = _flight_area->move_inside(_aim_pos, relaxed, drone.pos());
                     _control_mode = position_control;
                 } else if (!_res.via && interception_position_in_flightarea && stopping_position_in_flightarea && _res.valid) {
+                    _aim_pos += 0.4f * (_aim_pos - drone.pos()) / normf(_aim_pos - drone.pos());
                     _control_mode = position_control;
                 } else {
                     // Insect is currently not interceptable. Try to go in front of the target (and hope is target changing its path)
