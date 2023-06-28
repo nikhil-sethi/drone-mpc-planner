30a0200638c6fcfb4f3bded019558f85093387ae
Rik <rikjbouwmeester@gmail.com>
2023-06-28 14:44:17 +0200

    Match KIV safety margin with one used in optimizer

--- base/src/dronecontroller.cpp
+++ base/src/dronecontroller.cpp
@@ -809,10 +809,10 @@ cv::Point3f DroneController::kiv_update(TrackData data_drone) {
     bool flight_mode_with_kiv = _flight_mode == fm_flying_pid || _flight_mode == fm_reset_headless_yaw || _flight_mode == fm_correct_yaw;
 
     if (data_drone.pos_valid && data_drone.vel_valid && flight_mode_with_kiv && !(_time - start_takeoff_burn_time < 0.45)) {
-        if (control_mode_hold_filter.output()) //feedforward was active in the close past
-            return kiv_ctrl.correction_acceleration(data_drone, transmission_delay_duration, strict);
-        else
-            return kiv_ctrl.correction_acceleration(data_drone, transmission_delay_duration, relaxed);
+        // if (control_mode_hold_filter.output()) //feedforward was active in the close past
+        //     return kiv_ctrl.correction_acceleration(data_drone, transmission_delay_duration, strict);
+        // else
+        return kiv_ctrl.correction_acceleration(data_drone, transmission_delay_duration, relaxed);
     }
     else
         return {0, 0, 0};
