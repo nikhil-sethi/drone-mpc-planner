5b5f8bf1787ea6b5ef37c7750b73897e61abbbc7
Rik <rikjbouwmeester@gmail.com>
2023-06-29 15:00:49 +0200

    Revert "Tune thrust towards intermediate pos"
    This reverts commit 7dfe1082343241cfa90bc02bfc038d0b66cc6bb4.

--- base/src/navigation/interceptor.cpp
+++ base/src/navigation/interceptor.cpp
@@ -164,8 +164,8 @@ void Interceptor::update_aim_in_flightarea(rapid_route_result rapid_route_res) {
             stopping_position_in_flightarea = false;
 
         if (rapid_route_res.via && interception_position_in_flightarea && stopping_position_in_flightarea) {
-            if (normf(rapid_route_res.intermediate_position - _drone->tracker.last_track_data().pos()) > 0.1f)
-                _aim_pos = _drone->tracker.last_track_data().pos() + 0.8f * (rapid_route_res.intermediate_position - _drone->tracker.last_track_data().pos()) / normf(rapid_route_res.intermediate_position - _drone->tracker.last_track_data().pos());
+            if (normf(rapid_route_res.intermediate_position - _drone->tracker.last_track_data().pos()) > 0.25f)
+                _aim_pos = _drone->tracker.last_track_data().pos() + 0.5f * (rapid_route_res.intermediate_position - _drone->tracker.last_track_data().pos()) / normf(rapid_route_res.intermediate_position - _drone->tracker.last_track_data().pos());
             else
                 _aim_pos = rapid_route_res.intermediate_position;
             _n_frames_aim_not_in_range = 0;
