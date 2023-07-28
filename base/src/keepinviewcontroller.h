bb0d0702f6fb1cab23a1264ef307e9454254ce69
Rik <rikjbouwmeester@gmail.com>
2023-07-28 11:28:04 +0200

    Increase KIV safety factor (holidays mode)

--- base/src/keepinviewcontroller.h
+++ base/src/keepinviewcontroller.h
@@ -20,7 +20,7 @@ private:
 public:
     // Parameter:
     float drone_rotating_time;
-    const float safety = 1.5f;
+    const float safety = 2.f;
 
     // States:
     bool active = false;  //indicator for the log, whether kiv is active or not
