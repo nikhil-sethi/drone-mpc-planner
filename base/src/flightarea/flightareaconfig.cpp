a237c012e9b1a8e977daabe49db17d16bcb75e97
Rik <rikjbouwmeester@gmail.com>
2023-08-22 14:10:09 +0200

    [x] Move camera protector plane closer to camera
    Camera protector plane adheres camera normal angle. Systems where the drone pad is placed close to the camera saw the drone take off straight into this plane, resulting in a heavy KIV correction at take-off.

--- base/src/flightarea/flightareaconfig.cpp
+++ base/src/flightarea/flightareaconfig.cpp
@@ -11,7 +11,7 @@ void FlightAreaConfig::create_camera_planes() {
     _planes.push_back(Plane(1, view_data.point_right_bottom, view_data.point_right_top,    right_plane, _planes.size()));
 
     cv::Point3f camera_normal(cv::Point3f(0, -sinf(_cam->camera_pitch()* deg2rad), -cosf(_cam->camera_pitch()* deg2rad)));
-    add_plane(0.85f * camera_normal, camera_normal, camera_protector_plane);
+    add_plane(0.7f * camera_normal, camera_normal, camera_protector_plane);
     apply_safety_angle(1.f * deg2rad);
 }
 #endif
